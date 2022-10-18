1a6ced6d44d49b0c2ae85dbf919ac39a17ea1cba
Thomas Jespersen <tje@platformplatform.net>
2022-10-18 22:15:57 +0200

    Add Terraform state configuration

---
+++ cloud-infrastructure/terraform/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  # Setting the required_ Azure Provider and version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+
+  # Terrafrom storrage account (must be created before running Terrafrom)
+  backend "azurerm" {
+    storage_account_name = "__terraformstorageaccount__"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+    access_key           = "__storagekey__"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
