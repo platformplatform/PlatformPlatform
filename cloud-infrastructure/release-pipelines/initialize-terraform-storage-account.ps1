e41fd872a4dcfbbc8a23bc53df6ad92d4155ad33
Thomas Jespersen <tje@platformplatform.net>
2022-10-22 13:25:38 +0200

    Add PowerShell Cmdlet parameters to initialize Terraform storage account

--- cloud-infrastructure/release-pipelines/initialize-terraform-storage-account.ps1
+++ cloud-infrastructure/release-pipelines/initialize-terraform-storage-account.ps1
@@ -1,8 +1,11 @@
+[CmdletBinding()]
+param ($terraformlocation, $terraformresourcegroup, $terraformstorageaccount)
+
 # This will create the Azure resource group for Terraform if it does not exsist
-az group create --location $(terraformlocation) --name $(terraformresourcegroup)
+az group create --location $terraformlocation --name $terraformresourcegroup
 
 # This will create the Terraform storage account if it does not exsist
-az storage account create --name $(terraformstorageaccount) --resource-group $(terraformresourcegroup) --location $(terraformlocation) --sku Standard_LRS
+az storage account create --name $terraformstorageaccount --resource-group $terraformresourcegroup --location $terraformlocation --sku Standard_LRS
 
 # This will create the Terraform storage container if it does not exsist
-az storage container create --name terraform --account-name $(terraformstorageaccount)
+az storage container create --name terraform --account-name $terraformstorageaccount
