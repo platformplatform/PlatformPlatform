91e9aba152fca78b7e8eade13f286d29e752b954
Thomas Jespersen <tje@platformplatform.net>
2022-10-22 23:46:52 +0200

    Set CloudApp resource group lock to CanNotDelete

--- cloud-infrastructure/terraform/recource-group.tf
+++ cloud-infrastructure/terraform/recource-group.tf
@@ -1,5 +1,11 @@
 # Create a resource group
-resource "azurerm_resource_group" "CloudApp" {
+resource "azurerm_resource_group" "cloud-app" {
   name     = "CloudApp"
   location = "West Europe"
+}
+
+resource "azurerm_management_lock" "cloud-app-resource-group-lock" {
+  name       = "resource-group-lock"
+  scope      = azurerm_resource_group.cloud-app.id
+  lock_level = "CanNotDelete"
 }
