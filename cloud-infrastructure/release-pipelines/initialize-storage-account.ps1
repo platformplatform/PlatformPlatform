899827f3925c7249f5392bd70aca65267f3e0968
Thomas Jespersen <tje@platformplatform.net>
2022-10-23 22:00:15 +0200

    Rename variables in initialize-terraform-storage-account.ps1 to not include terraform and add container

---
+++ cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
@@ -0,0 +1,11 @@
+[CmdletBinding()]
+param ($location, $resourcegroup, $storageaccount, $container)
+
+# This will create an Azure resource group for the storgae account if it does not exsist
+az group create --location $location --name $resourcegroup
+
+# This will create a storage account if it does not exsist
+az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS
+
+# This will create a container if it does not exsist
+az storage container create --name $container --account-name $storageaccount
