8878433d39d69ef6571d2215f0d7029b964bc6b4
Thomas Jespersen <tje@platformplatform.net>
2022-10-30 00:50:01 +0200

    Create global Monitor resource group in West Europe

---
+++ cloud-infrastructure/terraform/global/monitor-recource-group.tf
@@ -0,0 +1,11 @@
+# Create a resource group
+resource "azurerm_resource_group" "monitor-resource-group" {
+  name     = "Monitor"
+  location = "westeu"
+}
+
+resource "azurerm_management_lock" "monitor-resource-group-lock" {
+  name       = "monitor-resource-group-lock"
+  scope      = azurerm_resource_group.monitor-resource-group.id
+  lock_level = "CanNotDelete"
+}
