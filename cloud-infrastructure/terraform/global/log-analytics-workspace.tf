dcd0d54fe3ae8ac8d402194e9bdc6c04c7c25a68
Thomas Jespersen <tje@platformplatform.net>
2022-10-30 13:32:05 +0100

    Create Log Analytics workspace in global Monitor resource group

---
+++ cloud-infrastructure/terraform/global/log-analytics-workspace.tf
@@ -0,0 +1,9 @@
+resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
+  name                       = "log-analytics-workspace"
+  resource_group_name        = azurerm_resource_group.monitor-resource-group
+  location                   = var.global-resource-location
+  sku                        = "PerGB2018"
+  retention_in_days          = "30"
+  internet_ingestion_enabled = "true"
+  internet_query_enabled     = "true"
+}
