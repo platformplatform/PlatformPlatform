791638d7c2e3c399096acf605db1cecf0ecc946e
Thomas Jespersen <tje@platformplatform.net>
2022-10-30 20:19:19 +0100

    Add Azure Container Apps environment per cluster linking it to global log analytics workspace

---
+++ cloud-infrastructure/terraform/cluster/container-app-environment.tf
@@ -0,0 +1,19 @@
+resource "azapi_resource" "container-apps-environment" {
+  name      = "container-apps-environment"
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
+  location  = var.cluster_location
+  parent_id = azurerm_resource_group.cluster-resource-group.id
+
+  body = jsonencode({
+    properties = {
+      appLogsConfiguration = {
+        destination = "log-analytics"
+        logAnalyticsConfiguration = {
+          customerId = data.azurerm_log_analytics_workspace.log-analytics-workspace.workspace_id
+          sharedKey  = data.azurerm_log_analytics_workspace.log-analytics-workspace.primary_shared_key
+        }
+      },
+      zoneRedundant = false
+    }
+  })
+}
---
+++ cloud-infrastructure/terraform/cluster/data-log-analytics-workspace.tf
@@ -0,0 +1,4 @@
+data "azurerm_log_analytics_workspace" "log-analytics-workspace" {
+  name                = "log-analytics-workspace"
+  resource_group_name = "Monitor"
+}
