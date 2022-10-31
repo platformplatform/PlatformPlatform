c48326204d8f4681778efb021bea49110f306ad6
Thomas Jespersen <tje@platformplatform.net>
2022-11-01 00:53:37 +0100

    Add virtual network plus subnet

---
+++ cloud-infrastructure/terraform/cluster/virtual-network.tf
@@ -0,0 +1,19 @@
+resource "azurerm_virtual_network" "virtual-network" {
+  name                = "virtual-network"
+  location            = var.cluster_location
+  resource_group_name = azurerm_resource_group.cluster-resource-group.name
+  address_space       = ["10.0.0.0/16"]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.cluster-resource-group.name
+  virtual_network_name = azurerm_virtual_network.virtual-network.name
+  address_prefixes     = ["10.0.0.0/23"]
+}
