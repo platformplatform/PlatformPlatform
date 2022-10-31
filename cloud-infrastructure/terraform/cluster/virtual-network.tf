32d5b0233f3be961060debe35abeaa80420cfab0
Thomas Jespersen <tje@platformplatform.net>
2022-11-01 00:54:19 +0100

    Add network watcher cluster resource group to avoid generic NetworkWatcherRG resource group

--- cloud-infrastructure/terraform/cluster/virtual-network.tf
+++ cloud-infrastructure/terraform/cluster/virtual-network.tf
@@ -17,3 +17,15 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.virtual-network.name
   address_prefixes     = ["10.0.0.0/23"]
 }
+
+resource "azurerm_network_watcher" "network_watcher" {
+  name                = "network-watcher"
+  location            = var.cluster_location
+  resource_group_name = azurerm_resource_group.cluster-resource-group.name
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = local.tags
+}
