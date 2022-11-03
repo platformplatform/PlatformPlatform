6adc355e7e3d27e9d9531fb99c29dc2819369edd
Thomas Jespersen <tje@platformplatform.net>
2022-11-03 13:49:23 +0100

    Add network-watcher as a dependency to virtual-network to avoid having a NetworkWatcherRG created

--- cloud-infrastructure/terraform/cluster/virtual-network.tf
+++ cloud-infrastructure/terraform/cluster/virtual-network.tf
@@ -4,6 +4,10 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = azurerm_resource_group.cluster_resource_group.name
   address_space       = ["10.0.0.0/16"]
 
+  depends_on = [
+    azurerm_network_watcher.network_watcher
+  ]
+
   lifecycle {
     prevent_destroy = true
   }
