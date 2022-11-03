6c25ad2417b24cde2536bbfd2db4ebbd70300927
Thomas Jespersen <tje@platformplatform.net>
2022-11-03 16:22:21 +0100

    Connect virtual network and key vault

--- cloud-infrastructure/terraform/cluster/key-valut.tf
+++ cloud-infrastructure/terraform/cluster/key-valut.tf
@@ -4,13 +4,14 @@ resource "azurerm_key_vault" "key_vault" {
   resource_group_name           = azurerm_resource_group.cluster_resource_group.name
   sku_name                      = "standard"
   tenant_id                     = data.azurerm_client_config.current.tenant_id
-  public_network_access_enabled = false
+  public_network_access_enabled = true
   soft_delete_retention_days    = 7
   purge_protection_enabled      = false
 
   network_acls {
     bypass                     = "AzureServices"
     default_action             = "Deny"
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
   }
 
   lifecycle {
--- cloud-infrastructure/terraform/cluster/virtual-network.tf
+++ cloud-infrastructure/terraform/cluster/virtual-network.tf
@@ -3,10 +3,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   location            = var.cluster_location
   resource_group_name = azurerm_resource_group.cluster_resource_group.name
   address_space       = ["10.0.0.0/16"]
-
-  depends_on = [
-    azurerm_network_watcher.network_watcher
-  ]
+  depends_on          = [azurerm_network_watcher.network_watcher]
 
   lifecycle {
     prevent_destroy = true
@@ -20,6 +17,7 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.cluster_resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.0.0/23"]
+  service_endpoints    = ["Microsoft.KeyVault"]
 }
 
 resource "azurerm_network_watcher" "network_watcher" {
