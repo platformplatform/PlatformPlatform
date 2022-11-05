75eb8e7549dc79fe0662bd19886ef6c443dfaccc
Thomas Jespersen <tje@platformplatform.net>
2022-11-05 21:12:36 +0100

    Add environment parameter to initialize-storage-account script and set it as tag on Azure resources

--- cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
+++ cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
@@ -1,11 +1,11 @@
 [CmdletBinding()]
-param ($location, $resourcegroup, $storageaccount, $container)
+param ($location, $resourcegroup, $storageaccount, $container, $environment)
 
 # This will create an Azure resource group for the storgae account if it does not exsist
-az group create --location $location --name $resourcegroup
+az group create --location $location --name $resourcegroup --tags managed-by=powershell environment=$environment
 
 # This will create a storage account if it does not exsist
-az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS --min-tls-version TLS1_2
+az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS --min-tls-version TLS1_2 --tags managed-by=powershell environment=$environment
 
 # This will create a container if it does not exsist
 az storage container create --name $container --account-name $storageaccount
