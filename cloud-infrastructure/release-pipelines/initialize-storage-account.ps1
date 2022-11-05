bbda2d60e7696817c6f1d9667501d8e3e1f7a41d
Thomas Jespersen <tje@platformplatform.net>
2022-11-05 21:13:13 +0100

    Configure storage-account to allow setting account level immutability property

--- cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
+++ cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
@@ -5,7 +5,7 @@ param ($location, $resourcegroup, $storageaccount, $container, $environment)
 az group create --location $location --name $resourcegroup --tags managed-by=powershell environment=$environment
 
 # This will create a storage account if it does not exsist
-az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS --min-tls-version TLS1_2 --tags managed-by=powershell environment=$environment
+az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS --enable-alw true --min-tls-version TLS1_2 --tags managed-by=powershell environment=$environment
 
 # This will create a container if it does not exsist
 az storage container create --name $container --account-name $storageaccount
