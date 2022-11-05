c916c3859ec67dd6f03b4660ff1eada3858a450f
Thomas Jespersen <tje@platformplatform.net>
2022-11-05 18:10:03 +0100

    Configure storage account to use TLS 1.2

--- cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
+++ cloud-infrastructure/release-pipelines/initialize-storage-account.ps1
@@ -5,7 +5,7 @@ param ($location, $resourcegroup, $storageaccount, $container)
 az group create --location $location --name $resourcegroup
 
 # This will create a storage account if it does not exsist
-az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS
+az storage account create --name $storageaccount --resource-group $resourcegroup --location $location --sku Standard_LRS --min-tls-version TLS1_2
 
 # This will create a container if it does not exsist
 az storage container create --name $container --account-name $storageaccount
