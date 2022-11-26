294264936d27adc558a6f182db6a3445203335dd
Thomas Jespersen <tje@platformplatform.net>
2022-11-26 13:29:33 +0100

    Make shell script parameterized to allow what-if mode only

--- cloud-infrastructure/bicep/shared/create.sh
+++ cloud-infrastructure/bicep/shared/create.sh
@@ -1,4 +1,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 az group create -n $resourceGroupName -l "$location" --output table --tags environment=$environment
-az deployment group create --what-if -g $resourceGroupName --mode complete --output table -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
-az deployment group create           -g $resourceGroupName --mode complete --output table -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
+
+az deployment group create --what-if -g $resourceGroupName --mode complete -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
+
+if [[ "$1" == "apply" ]]
+then
+    az deployment group create --output table -g $resourceGroupName --mode complete -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
+fi
