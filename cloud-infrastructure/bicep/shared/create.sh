1c10f794bc45448960f421180950b6cc969355f2
Thomas Jespersen <tje@platformplatform.net>
2022-11-27 12:29:29 +0100

    Change deployment script to run in subscription scope and not resource group scope

--- cloud-infrastructure/bicep/shared/create.sh
+++ cloud-infrastructure/bicep/shared/create.sh
@@ -1,9 +1,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
-az group create -n $resourceGroupName -l "$location" --output table --tags environment=$environment
 
-az deployment group create --what-if -g $resourceGroupName --mode complete -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
+az deployment sub create --what-if -l "$location" --output table -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
 
 if [[ "$1" == "apply" ]]
 then
-    az deployment group create --output table -g $resourceGroupName --mode complete -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
+    az deployment sub create       -l "$location" --output table -f ./main.bicep -p containerRegistryName=$containerRegistryName environment=$environment
 fi
