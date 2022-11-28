d1de57a4388deb2008bfe92e77771984fdec3f9d
Thomas Jespersen <tje@platformplatform.net>
2022-11-28 17:33:13 +0100

    Add testing-east-us cluster configuration and script

---
+++ cloud-infrastructure/bicep/cluster/config/testing-east-us.sh
@@ -0,0 +1,8 @@
+environment="testing"
+location="East US"
+locationPrefix="east-us"
+resourceGroupName="testing-east-us"
+clusterUniqueName="mentumtestuse"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+. ../deploy.sh
