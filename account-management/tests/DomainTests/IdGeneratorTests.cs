a68b7048bc3e4670d8e399567e42cc986aa1fead
Thomas Jespersen <tje@platformplatform.net>
2023-04-03 17:27:19 +0200

    Add tests for IdGenerator class

---
+++ account-management/tests/DomainTests/IdGeneratorTests.cs
@@ -0,0 +1,34 @@
+namespace PlatformPlatform.AccountManagement.Tests;
+
+public class IdGeneratorTests
+{
+    [Fact]
+    public void NewId_GeneratesUniqueIds()
+    {
+        // Arrange
+        var idCount = 1000;
+        var generatedIds = new HashSet<long>();
+
+        // Act
+        for (var i = 0; i < idCount; i++) generatedIds.Add(IdGenerator.NewId());
+
+        // Assert
+        generatedIds.Count.Should().Be(idCount);
+    }
+
+    [Fact]
+    public void NewId_GeneratesIncreasingIds()
+    {
+        // Arrange
+        var idCount = 1000;
+        var previousId = 0L;
+
+        // Act & Assert
+        for (var i = 0; i < idCount; i++)
+        {
+            var currentId = IdGenerator.NewId();
+            currentId.Should().BeGreaterThan(previousId);
+            previousId = currentId;
+        }
+    }
+}
