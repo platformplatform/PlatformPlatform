2a35746083b617fdb45a98968ca4bd950805a823
Thomas Jespersen <tje@platformplatform.net>
2023-04-08 15:58:52 +0200

    Implement TenantRepository in infrastructure to add and read from SQL Server

---
+++ account-management/Infrastructure/Tenants/TenantRepository.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformPlatform.AccountManagement.Domain.Tenants;
+
+namespace PlatformPlatform.AccountManagement.Infrastructure.Tenants;
+
+public class TenantRepository : ITenantRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+    private readonly DbSet<Tenant> _tenantDbSet;
+
+    public TenantRepository(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+        _tenantDbSet = applicationDbContext.Tenants;
+    }
+
+    public async Task<Tenant?> GetByIdAsync(long id, CancellationToken cancellationToken)
+    {
+        return await _tenantDbSet.FindAsync(id, cancellationToken);
+    }
+
+    public async Task AddAsync(Tenant tenant, CancellationToken cancellationToken)
+    {
+        await _tenantDbSet.AddAsync(tenant, cancellationToken);
+        await _applicationDbContext.SaveChangesAsync(cancellationToken);
+    }
+}
