fff95b4cad333a85f5b60e1a8a0e07036d7cf251
Thomas Jespersen <tje@platformplatform.net>
2023-04-10 14:02:42 +0200

    Fix the class name for ApplicationDbContextModelSnapshot and rename file cs file to match MigrationId

--- account-management/Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ account-management/Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -10,11 +10,10 @@ using PlatformPlatform.AccountManagement.Infrastructure;
 namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class AccountManagementDbContextModelSnapshot : ModelSnapshot
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
-
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.4")
