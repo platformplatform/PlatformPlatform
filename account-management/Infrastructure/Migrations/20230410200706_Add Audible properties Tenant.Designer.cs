f7901affda92ff4221b4beee821eade0f9ad967b
Thomas Jespersen <tje@platformplatform.net>
2023-04-10 23:05:53 +0200

    Add Database migrations to add CreatedAt and UpdatedAt timestamps to Tenant table

--- account-management/Infrastructure/Migrations/20230410200706_Add Audible properties Tenant.Designer.cs
+++ account-management/Infrastructure/Migrations/20230410200706_Add Audible properties Tenant.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlatformPlatform.AccountManagement.Infrastructure;
 
@@ -11,9 +12,11 @@ using PlatformPlatform.AccountManagement.Infrastructure;
 namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230410200706_Add Audible properties Tenant")]
+    partial class AddAudiblepropertiesTenant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ account-management/Infrastructure/Migrations/20230410200706_Add Audible properties Tenant.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAudiblepropertiesTenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Tenants",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Tenants",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Tenants");
+        }
+    }
+}
