c2f34ecaff2b612ffe0753c5a39d33519354a057
Thomas Jespersen <tje@platformplatform.net>
2023-04-27 13:33:42 +0200

    Update documentation of IdGenerator, IAggregatRoot, IUnitOfWork, IRepository, StronglyTypedId to add more reasoning and background information

--- account-management/src/Domain/Primitives/IAggregateRoot.cs
+++ account-management/src/Domain/Primitives/IAggregateRoot.cs
@@ -1,10 +1,13 @@
 namespace PlatformPlatform.AccountManagement.Domain.Primitives;
 
 /// <summary>
-///     Interface for an aggregate root, which also implements IAuditableEntity.
-///     Aggregate roots are a DDD concept. An Aggregate is an entity but only some entities are aggregates.
-///     In DDD Repositories are used for read and write Aggregates to the database. E.g. when an aggregate
-///     is deleted all entities belong to the aggregate are deleted as well.
+///     Interface for aggregate roots, which also implements IAuditableEntity. Aggregate roots are a concept in
+///     Domain-Driven Design (DDD). An aggregate is an entity, but only some entities are aggregates. For example, an
+///     Order in an e-commerce system is an aggregate, but an OrderLine is not. An aggregate is a cluster of associated
+///     objects that are treated as a unit.
+///     In DDD, Repositories are used to read and write aggregates in the database. For example, when an aggregate is
+///     deleted, all entities belonging to the aggregate are deleted as well. Also, only aggregates can be fetched from
+///     the database, while entities that are not aggregates cannot (fetch the aggregate to get access to the entities).
 /// </summary>
 public interface IAggregateRoot : IAuditableEntity
 {
--- account-management/src/Domain/Primitives/IRepository.cs
+++ account-management/src/Domain/Primitives/IRepository.cs
@@ -3,9 +3,13 @@ namespace PlatformPlatform.AccountManagement.Domain.Primitives;
 /// <summary>
 ///     IRepository is a generic interface for repositories. Repositories are a DDD concept, and are used
 ///     as an abstraction over the database. In DDD, repositories are used to persist <see cref="IAggregateRoot" />.
-///     <see cref="Entity{T}" /> that are not aggregates cannot be fetched from the database using a repository.
-///     Repositories are not responsible for saving the entities to the database, which is handled by the
-///     <see cref="IUnitOfWork" />.
+///     <see cref="Entity{T}" /> that are not aggregates cannot be fetched from the database using a repository. They
+///     must be fetched indirectly by fetching the aggregate that they belong to. E.g., to fetch an OrderLine, you must
+///     fetch the Order that it belongs to.
+///     Repositories are not responsible for commiting the changes to the database, which is handled by the
+///     <see cref="IUnitOfWork" />. What this means is that when you add, update, or delete aggregates, they are just
+///     marked to be added, updated, or deleted, and it's not until the <see cref="IUnitOfWork" /> is committed that the
+///     changes are actually persisted to the database.
 /// </summary>
 public interface IRepository<T, in TId> where T : IAggregateRoot where TId : IComparable<TId>
 {
