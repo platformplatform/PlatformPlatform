75b256ca410e1c6508a8377df9849da4e041a8f0
Thomas Jespersen <tje@platformplatform.net>
2023-04-30 16:54:57 +0200

    Refactor CreateTenantCommandValidation by extracting ValidateSubdomain method

--- account-management/src/Application/Tenants/Commands/CreateTenant/CreateTenantCommandValidator.cs
+++ account-management/src/Application/Tenants/Commands/CreateTenant/CreateTenantCommandValidator.cs
@@ -6,15 +6,21 @@ namespace PlatformPlatform.AccountManagement.Application.Tenants.Commands.Create
 
 public sealed class CreateTenantCommandValidator : TenantCommandValidatorBase<CreateTenantCommand>
 {
+    private readonly ITenantRepository _tenantRepository;
+
     public CreateTenantCommandValidator(ITenantRepository tenantRepository)
     {
+        _tenantRepository = tenantRepository;
         RuleFor(x => x.Subdomain)
             .Length(TenantValidationConstants.SubdomainMinLength, TenantValidationConstants.SubdomainMaxLength)
             .Matches(new Regex("^[a-z0-9]*$"))
             .WithMessage("The subdomain must contain only lowercase letters and numbers.")
-            .MustAsync(async (subdomain, cancellationToken) =>
-                await tenantRepository.IsSubdomainFreeAsync(subdomain, cancellationToken)
-            )
+            .MustAsync(ValidateSubdomain)
             .WithMessage("The subdomain must be unique.");
+    }
+
+    private async Task<bool> ValidateSubdomain(string subdomain, CancellationToken cancellationToken)
+    {
+        return await _tenantRepository.IsSubdomainFreeAsync(subdomain, cancellationToken);
     }
 }
