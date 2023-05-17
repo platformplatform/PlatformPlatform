6aa546ab1c28e8a6011b98da6c109d660a78adc0
Thomas Jespersen <tje@platformplatform.net>
2023-05-17 18:08:32 +0200

    Add test for UnitOfWork to verify accurate updating of CreateAt and ModifiedAt timestamps

---
+++ account-management/tests/Foundation.Tests/Infrastructure/UnitOfWorkTests.cs
@@ -0,0 +1,59 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using PlatformPlatform.Foundation.Domain;
+using PlatformPlatform.Foundation.Infrastructure;
+using PlatformPlatform.Foundation.Tests.TestEntities;
+using Xunit;
+
+namespace PlatformPlatform.Foundation.Tests.Infrastructure;
+
+public class UnitOfWorkTests
+{
+    private readonly TestDbContext _testDbContext;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UnitOfWorkTests()
+    {
+        var options = new DbContextOptionsBuilder<TestDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        _testDbContext = new TestDbContext(options);
+        _unitOfWork = new UnitOfWork(_testDbContext);
+    }
+
+    [Fact]
+    public async Task CommitAsync_WhenAggregateIsAdded_ShouldSetCreatedAt()
+    {
+        // Arrange
+        var newTestAggregate = TestAggregate.Create("TestAggregate");
+
+        // Act
+        _testDbContext.TestAggregates.Add(newTestAggregate);
+        newTestAggregate.ClearDomainEvents(); // Simulate that domain events have been handled
+        await _unitOfWork.CommitAsync(CancellationToken.None);
+
+        // Assert
+        newTestAggregate.CreatedAt.Should().NotBe(default);
+        newTestAggregate.ModifiedAt.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task CommitAsync_WhenAggregateIsModified_ShouldUpdateModifiedAt()
+    {
+        // Arrange
+        var newTestAggregate = TestAggregate.Create("TestAggregate");
+        _testDbContext.TestAggregates.Add(newTestAggregate);
+        newTestAggregate.ClearDomainEvents(); // Simulate that domain events have been handled
+        await _unitOfWork.CommitAsync(CancellationToken.None);
+        var initialCreatedAt = newTestAggregate.CreatedAt;
+
+        // Act
+        newTestAggregate.Name = "UpdatedTestAggregate";
+        await _unitOfWork.CommitAsync(CancellationToken.None);
+
+        // Assert
+        newTestAggregate.ModifiedAt.Should().NotBeNull();
+        newTestAggregate.ModifiedAt.Should().BeAfter(initialCreatedAt);
+        newTestAggregate.CreatedAt.Should().Be(initialCreatedAt);
+    }
+}
