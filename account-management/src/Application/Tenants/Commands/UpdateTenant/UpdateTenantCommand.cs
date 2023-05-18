3d0ec4c58f447db5d96cbbec2fd3cd03660daecf
Thomas Jespersen <tje@platformplatform.net>
2023-05-18 20:13:53 +0200

    Implement UpdateTenantCommand and corresponding WebAPI endpoint

---
+++ account-management/src/Application/Tenants/Commands/UpdateTenant/UpdateTenantCommand.cs
@@ -0,0 +1,48 @@
+using JetBrains.Annotations;
+using Mapster;
+using MediatR;
+using PlatformPlatform.AccountManagement.Application.Tenants.Dtos;
+using PlatformPlatform.AccountManagement.Domain.Tenants;
+using PlatformPlatform.Foundation.Application;
+using PlatformPlatform.Foundation.Domain;
+
+namespace PlatformPlatform.AccountManagement.Application.Tenants.Commands.UpdateTenant;
+
+public sealed record UpdateTenantCommand(TenantId TenantId, string Name, string Email, string? Phone)
+    : IRequest<CommandResult<TenantDto>>;
+
+[UsedImplicitly]
+public sealed class UpdateTenantCommandHandler : IRequestHandler<UpdateTenantCommand, CommandResult<TenantDto>>
+{
+    private readonly ITenantRepository _tenantRepository;
+
+    public UpdateTenantCommandHandler(ITenantRepository tenantRepository)
+    {
+        _tenantRepository = tenantRepository;
+    }
+
+    public async Task<CommandResult<TenantDto>> Handle(UpdateTenantCommand command, CancellationToken cancellationToken)
+    {
+        var tenant = await _tenantRepository.GetByIdAsync(command.TenantId, cancellationToken);
+        if (tenant is null)
+        {
+            return CommandResult<TenantDto>.Failure(new[] {new PropertyError("TenantId", "Tenant not found.")});
+        }
+
+        var propertyErrors = TenantValidation.ValidateName(command.Name).Errors
+            .Concat(TenantValidation.ValidateEmail(command.Email).Errors)
+            .Concat(TenantValidation.ValidatePhone(command.Phone).Errors)
+            .ToArray();
+
+        if (propertyErrors.Any())
+        {
+            return CommandResult<TenantDto>.Failure(propertyErrors);
+        }
+
+        tenant.Update(command.Name, command.Email, command.Phone);
+
+        _tenantRepository.Update(tenant);
+
+        return tenant.Adapt<TenantDto>();
+    }
+}
--- account-management/src/WebApi/Endpoints/TenantEndpoints.cs
+++ account-management/src/WebApi/Endpoints/TenantEndpoints.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using PlatformPlatform.AccountManagement.Application.Tenants.Commands.CreateTenant;
 using PlatformPlatform.AccountManagement.Application.Tenants.Commands.DeleteTenant;
+using PlatformPlatform.AccountManagement.Application.Tenants.Commands.UpdateTenant;
 using PlatformPlatform.AccountManagement.Application.Tenants.Queries;
 using PlatformPlatform.AccountManagement.Domain.Tenants;
 
@@ -13,6 +14,7 @@ public static class TenantEndpoints
         var group = routes.MapGroup("/tenants");
         group.MapGet("/{id}", GetTenant);
         group.MapPost("/", CreateTenant);
+        group.MapPut("/{id}", UpdateTenant);
         group.MapDelete("/{id}", DeleteTenant);
     }
 
@@ -32,6 +34,15 @@ public static class TenantEndpoints
             : Results.BadRequest(createTenantCommandResult.Errors);
     }
 
+    private static async Task<IResult> UpdateTenant(string id, UpdateTenantRequest request, ISender sender)
+    {
+        var command = new UpdateTenantCommand(TenantId.FromString(id), request.Name, request.Email, request.Phone);
+        var result = await sender.Send(command);
+        return result.IsSuccess
+            ? Results.Ok(result.Value)
+            : Results.BadRequest(result.Errors);
+    }
+
     private static async Task<IResult> DeleteTenant(string id, ISender sender)
     {
         var result = await sender.Send(new DeleteTenantCommand(TenantId.FromString(id)));
@@ -40,3 +51,5 @@ public static class TenantEndpoints
             : Results.BadRequest(result.Errors);
     }
 }
+
+public sealed record UpdateTenantRequest(string Name, string Email, string? Phone);
