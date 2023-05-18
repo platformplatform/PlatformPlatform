7ef348ea91f82863328e0d3e90547c4f298d1021
Thomas Jespersen <tje@platformplatform.net>
2023-05-18 23:15:23 +0200

    Add an extension method in Foundation to convert a CommandResult<T> to a valid AspNetCore.Http.IResult

---
+++ account-management/src/Foundation/WebApi/CommandResultExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using PlatformPlatform.Foundation.Application;
+
+namespace PlatformPlatform.Foundation.WebApi;
+
+public static class CommandResultExtensions
+{
+    public static IResult AsHttpResult<T>(this CommandResult<T> result)
+    {
+        return Results.Json(result.IsSuccess ? result.Value : result.Errors, statusCode: (int) result.StatusCode);
+    }
+
+    public static IResult AsHttpResult<T>(this CommandResult<T> result, string uri)
+    {
+        return result.IsSuccess
+            ? Results.Created(uri, result.Value)
+            : Results.Json(result.Errors, statusCode: (int) result.StatusCode);
+    }
+}
