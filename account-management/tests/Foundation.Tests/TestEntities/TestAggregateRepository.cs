4bf88648641f2cdb93df2cf8f8566adfcbe857d2
Thomas Jespersen <tje@platformplatform.net>
2023-05-18 16:09:23 +0200

    Change TestAggregateRepository to utilize TestDbContext.TestAggregates instead of testDbContext.Set<TestAggregate>

--- account-management/tests/Foundation.Tests/TestEntities/TestAggregateRepository.cs
+++ account-management/tests/Foundation.Tests/TestEntities/TestAggregateRepository.cs
@@ -5,30 +5,30 @@ namespace PlatformPlatform.Foundation.Tests.TestEntities;
 
 public class TestAggregateRepository : IRepository<TestAggregate, long>
 {
-    private readonly DbSet<TestAggregate> _dbSet;
+    private readonly DbSet<TestAggregate> _testAggregatesDbSet;
 
-    public TestAggregateRepository(DbContext testDbContext)
+    public TestAggregateRepository(TestDbContext testDbContext)
     {
-        _dbSet = testDbContext.Set<TestAggregate>();
+        _testAggregatesDbSet = testDbContext.TestAggregates;
     }
 
     public async Task<TestAggregate?> GetByIdAsync(long id, CancellationToken cancellationToken)
     {
-        return await _dbSet.FindAsync(new object?[] {id}, cancellationToken);
+        return await _testAggregatesDbSet.FindAsync(new object?[] {id}, cancellationToken);
     }
 
     public void Add(TestAggregate aggregate)
     {
-        _dbSet.Add(aggregate);
+        _testAggregatesDbSet.Add(aggregate);
     }
 
     public void Update(TestAggregate aggregate)
     {
-        _dbSet.Update(aggregate);
+        _testAggregatesDbSet.Update(aggregate);
     }
 
     public void Remove(TestAggregate aggregate)
     {
-        _dbSet.Remove(aggregate);
+        _testAggregatesDbSet.Remove(aggregate);
     }
 }
