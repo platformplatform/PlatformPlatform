30260813ca4eebd5c7ad153c2976a46cec995f78
Thomas Jespersen <tje@platformplatform.net>
2023-05-29 17:41:30 +0200

    Add new CreateUser.Command, Handler, and Validator including a reusable UserValidator

---
+++ account-management/Application/Users/Commands/UserValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using JetBrains.Annotations;
+
+namespace PlatformPlatform.AccountManagement.Application.Users.Commands;
+
+public interface IUserValidation
+{
+    string Email { get; }
+}
+
+[UsedImplicitly]
+public abstract class UserValidator<T> : AbstractValidator<T> where T : IUserValidation
+{
+    protected UserValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+    }
+}
