b36da4f56d4a017f0ae976a2e2941b52be2d6f6a
Thomas Jespersen <tje@platformplatform.net>
2023-05-29 20:05:28 +0200

    Change creation of TenantOwner to happen in the CreateTenant.Command instead of a TenantCreatedEventHandler

--- account-management/Application/Tenants/Commands/CreateTenant.cs
+++ account-management/Application/Tenants/Commands/CreateTenant.cs
@@ -1,7 +1,9 @@
 using FluentValidation;
 using JetBrains.Annotations;
 using MediatR;
+using PlatformPlatform.AccountManagement.Application.Users.Commands;
 using PlatformPlatform.AccountManagement.Domain.Tenants;
+using PlatformPlatform.AccountManagement.Domain.Users;
 using PlatformPlatform.SharedKernel.ApplicationCore.Cqrs;
 
 namespace PlatformPlatform.AccountManagement.Application.Tenants.Commands;
@@ -14,25 +16,41 @@ public static class CreateTenant
     [UsedImplicitly]
     public sealed class Handler : IRequestHandler<Command, Result<Tenant>>
     {
+        private readonly ISender _mediator;
         private readonly ITenantRepository _tenantRepository;
 
-        public Handler(ITenantRepository tenantRepository)
+        public Handler(ITenantRepository tenantRepository, ISender mediator)
         {
             _tenantRepository = tenantRepository;
+            _mediator = mediator;
         }
 
         public async Task<Result<Tenant>> Handle(Command command, CancellationToken cancellationToken)
         {
             var tenant = Tenant.Create(command.Name, command.Subdomain, command.Email, command.Phone);
             await _tenantRepository.AddAsync(tenant, cancellationToken);
+
+            await CreateTenantOwner(command.Email, cancellationToken);
             return tenant;
+        }
+
+        private async Task CreateTenantOwner(string tenantOwnerEmail, CancellationToken cancellationToken)
+        {
+            var createTenantOwnerUserCommand = new CreateUser.Command(tenantOwnerEmail, UserRole.TenantOwner);
+            var result = await _mediator.Send(createTenantOwnerUserCommand, cancellationToken);
+
+            if (!result.IsSuccess)
+            {
+                throw new InvalidOperationException(
+                    $"Failed to create a TenantOwner user for tenant. Reason: {result.ErrorMessage}");
+            }
         }
     }
 
     [UsedImplicitly]
     public sealed class Validator : TenantValidator<Command>
     {
-        public Validator(ITenantRepository repository)
+        public Validator(ITenantRepository repository, IUserRepository userRepository)
         {
             RuleFor(x => x.Subdomain).NotEmpty();
             RuleFor(x => x.Subdomain)
@@ -41,6 +59,11 @@ public static class CreateTenant
                 .MustAsync(async (subdomain, token) => await repository.IsSubdomainFreeAsync(subdomain, token))
                 .WithMessage("The subdomain is not available.")
                 .When(x => !string.IsNullOrEmpty(x.Subdomain));
+
+            RuleFor(x => x.Email)
+                .MustAsync(async (email, token) => await userRepository.IsEmailFreeAsync(email, token))
+                .WithMessage(x => $"The email '{x.Email}' is already in use by another user.")
+                .When(x => !string.IsNullOrEmpty(x.Email));
         }
     }
 }
--- account-management/Application/Tenants/EventHandlers/TenantCreatedEventHandler.cs
+++ account-management/Application/Tenants/EventHandlers/TenantCreatedEventHandler.cs
@@ -1,9 +1,7 @@
 using JetBrains.Annotations;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using PlatformPlatform.AccountManagement.Application.Users.Commands;
 using PlatformPlatform.AccountManagement.Domain.Tenants;
-using PlatformPlatform.AccountManagement.Domain.Users;
 
 namespace PlatformPlatform.AccountManagement.Application.Tenants.EventHandlers;
 
@@ -11,27 +9,17 @@ namespace PlatformPlatform.AccountManagement.Application.Tenants.EventHandlers;
 public sealed class TenantCreatedEventHandler : INotificationHandler<TenantCreatedEvent>
 {
     private readonly ILogger<TenantCreatedEventHandler> _logger;
-    private readonly ISender _mediator;
     private readonly ITenantRepository _tenantRepository;
 
-    public TenantCreatedEventHandler(ILogger<TenantCreatedEventHandler> logger, ITenantRepository tenantRepository,
-        ISender mediator)
+    public TenantCreatedEventHandler(ILogger<TenantCreatedEventHandler> logger, ITenantRepository tenantRepository)
     {
         _logger = logger;
         _tenantRepository = tenantRepository;
-        _mediator = mediator;
     }
 
-    public async Task Handle(TenantCreatedEvent tenantCreatedEvent, CancellationToken cancellationToken)
+    public async Task Handle(TenantCreatedEvent notification, CancellationToken cancellationToken)
     {
-        var tenant = (await _tenantRepository.GetByIdAsync(tenantCreatedEvent.TenantId, cancellationToken))!;
-        var createUserOwnerCommand = new CreateUser.Command(tenant.Email, UserRole.TenantOwner);
-        var result = await _mediator.Send(createUserOwnerCommand, cancellationToken);
-
-        if (!result.IsSuccess)
-        {
-            throw new Exception($"Failed to create a user for tenant {tenant.Id}. Reason: {result.ErrorMessage}");
-        }
+        var tenant = (await _tenantRepository.GetByIdAsync(notification.TenantId, cancellationToken))!;
 
         _logger.LogInformation(@"Raise event to send Welcome mail to {TenantName}", tenant.Name);
     }
