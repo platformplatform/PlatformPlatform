cb38411c68556e58112aea52362201bac0d083f8
Thomas Jespersen <tje@platformplatform.net>
2023-05-29 23:01:35 +0200

    Add Database migration for Adding Users table

---
+++ account-management/Infrastructure/Migrations/20230529195123_Add_Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public sealed partial class Add_Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+    }
+}
