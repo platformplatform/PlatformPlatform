520cdb48e5505c3d5ad54167ced20fd40d94d17a
Thomas Jespersen <tje@platformplatform.net>
2023-06-05 18:55:17 +0200

    Add a new IIdentity interface to allow accessing the Entity.Id without relying on generic types

--- shared-kernel/DomainCore/Entities/Entity.cs
+++ shared-kernel/DomainCore/Entities/Entity.cs
@@ -10,7 +10,8 @@ namespace PlatformPlatform.SharedKernel.DomainCore.Entities;
 ///     If two entities have the same identity, they are considered to be the same entity.
 ///     It is recommended to use a <see cref="StronglyTypedId{T}" /> for the ID to make the domain more meaningful.
 /// </summary>
-public abstract class Entity<T> : IEquatable<Entity<T>> where T : IComparable<T>
+public abstract class Entity<T> : IIdentity, IEquatable<Entity<T>>
+    where T : IComparable<T>
 {
     protected Entity(T id)
     {
@@ -27,6 +28,8 @@ public abstract class Entity<T> : IEquatable<Entity<T>> where T : IComparable<T>
                (ReferenceEquals(this, other) ||
                 EqualityComparer<T>.Default.Equals(Id, other.Id));
     }
+
+    object IIdentity.Id => Id;
 
     public override bool Equals(object? obj)
     {
---
+++ shared-kernel/DomainCore/Entities/IIdentity.cs
@@ -0,0 +1,6 @@
+namespace PlatformPlatform.SharedKernel.DomainCore.Entities;
+
+public interface IIdentity
+{
+    object Id { get; }
+}
