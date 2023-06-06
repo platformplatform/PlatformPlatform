c269a712a147895c30c1ecbaf941f621bf1e8434
Thomas Jespersen <tje@platformplatform.net>
2023-06-06 21:10:34 +0200

    Create a generic type converter to support the use of strongly typed IDs in URL routes

---
+++ shared-kernel/DomainCore/Identity/StronglyTypedIdTypeConverter.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+
+namespace PlatformPlatform.SharedKernel.DomainCore.Identity;
+
+public abstract class StronglyTypedIdTypeConverter<T> : TypeConverter where T : StronglyTypedId<T>
+{
+    private static readonly MethodInfo? TryParseMethod = typeof(T).GetMethod("TryParse");
+
+    public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+    {
+        if (value is not string valueAsString || TryParseMethod == null)
+        {
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        var parameters = new object?[] {valueAsString, null};
+
+        if ((bool) TryParseMethod.Invoke(null, parameters)!)
+        {
+            return (T?) parameters[1];
+        }
+
+        return base.ConvertFrom(context, culture, value);
+    }
+}
