9f63ceff099371afffb6b1b8ef69f696c0ec3583
Thomas Jespersen <tje@platformplatform.net>
2023-06-07 18:59:59 +0200

    Create non-generic Result and ApiResults classes. Update Created Result to retrieve the created ID directly from the result

--- shared-kernel/ApiCore/HttpResults/ApiResult.cs
+++ shared-kernel/ApiCore/HttpResults/ApiResult.cs
@@ -3,13 +3,74 @@ using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlatformPlatform.SharedKernel.ApplicationCore.Cqrs;
-using PlatformPlatform.SharedKernel.DomainCore.Entities;
 using IResult = Microsoft.AspNetCore.Http.IResult;
 
 namespace PlatformPlatform.SharedKernel.ApiCore.HttpResults;
 
+public partial class ApiResult : IResult
+{
+    private readonly Result _result;
+    private readonly string? _routePrefix;
+
+    public ApiResult(Result result, string? routePrefix = null)
+    {
+        _result = result;
+        _routePrefix = routePrefix;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        return _routePrefix == null
+            ? ConvertResult().ExecuteAsync(httpContext)
+            : ConvertResult(_routePrefix).ExecuteAsync(httpContext);
+    }
+
+    private IResult ConvertResult(string? routePrefix = null)
+    {
+        if (!_result.IsSuccess) return GetProblemDetailsAsJson();
+
+        return routePrefix == null
+            ? Results.Ok()
+            : Results.Created($"{routePrefix}/{_result}", null);
+    }
+
+    private IResult GetProblemDetailsAsJson()
+    {
+        return Results.Json(CreateProblemDetails(), statusCode: (int) _result.StatusCode);
+    }
+
+    private ProblemDetails CreateProblemDetails()
+    {
+        var statusCode = _result.StatusCode;
+        var problemDetails = new ProblemDetails
+        {
+            Type = $"https://httpstatuses.com/{(int) _result.StatusCode}",
+            Title = SplitCamelCaseTitle(statusCode.ToString()),
+            Status = (int) _result.StatusCode
+        };
+
+        if (_result.ErrorMessage is not null) problemDetails.Detail = _result.ErrorMessage.Message;
+
+        if (_result.Errors?.Length > 0) problemDetails.Extensions[nameof(_result.Errors)] = _result.Errors;
+
+        return problemDetails;
+    }
+
+    private static string SplitCamelCaseTitle(string title)
+    {
+        return SplitCamelCase().Replace(title, " $1");
+    }
+
+    [GeneratedRegex("(?<=[a-z])([A-Z])", RegexOptions.Compiled)]
+    private static partial Regex SplitCamelCase();
+
+    public static implicit operator ApiResult(Result result)
+    {
+        return new ApiResult(result);
+    }
+}
+
 public partial class ApiResult<T> : IResult
-    where T : IIdentity
 {
     private readonly Result<T> _result;
     private readonly string? _routePrefix;
@@ -33,7 +94,7 @@ public partial class ApiResult<T> : IResult
 
         return routePrefix == null
             ? Results.Ok(_result.Value!.Adapt<T>())
-            : Results.Created($"{routePrefix}/{_result.Value!.GetId()}", null);
+            : Results.Created($"{routePrefix}/{_result.Value}", null);
     }
 
     private IResult GetProblemDetailsAsJson()
--- shared-kernel/ApplicationCore/Cqrs/Result.cs
+++ shared-kernel/ApplicationCore/Cqrs/Result.cs
@@ -26,6 +26,58 @@ public interface IResult
 ///     an <see cref="ErrorMessage" /> or a collection of a <see cref="ErrorMessage" />. In both successful and
 ///     non-successful cases, the <see cref="Result{T}" /> will contain a status code.
 /// </summary>
+public class Result : IResult
+{
+    protected Result(HttpStatusCode httpStatusCode)
+    {
+        IsSuccess = true;
+        StatusCode = httpStatusCode;
+    }
+
+    [UsedImplicitly]
+    public Result(HttpStatusCode statusCode, ErrorMessage errorMessage, ErrorDetail[] errors)
+    {
+        IsSuccess = false;
+        StatusCode = statusCode;
+        ErrorMessage = errorMessage;
+        Errors = errors;
+    }
+
+    public bool IsSuccess { get; }
+
+    public HttpStatusCode StatusCode { get; }
+
+    public ErrorMessage? ErrorMessage { get; }
+
+    public ErrorDetail[]? Errors { get; }
+
+    public string GetErrorSummary()
+    {
+        return ErrorMessage?.Message
+               ?? string.Join(Environment.NewLine, Errors!.Select(ed => $"{ed.Code}: {ed.Message}"));
+    }
+
+    public static Result NotFound(string message)
+    {
+        return new Result(HttpStatusCode.NotFound, new ErrorMessage(message), Array.Empty<ErrorDetail>());
+    }
+
+    public static Result BadRequest(string message)
+    {
+        return new Result(HttpStatusCode.BadRequest, new ErrorMessage(message), Array.Empty<ErrorDetail>());
+    }
+
+    public static Result NoContent()
+    {
+        return new Result(HttpStatusCode.NoContent);
+    }
+
+    public static Result Success()
+    {
+        return new Result(HttpStatusCode.OK);
+    }
+}
+
 public class Result<T> : IResult
 {
     private Result(T value, HttpStatusCode httpStatusCode)
