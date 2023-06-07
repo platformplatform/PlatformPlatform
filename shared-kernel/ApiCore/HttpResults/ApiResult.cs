d29c8d90331849162b94ce8e7db82a4c156943c7
Thomas Jespersen <tje@platformplatform.net>
2023-06-07 22:44:02 +0200

    Refactor ApiResult<T> to inherit from ApiResult

--- shared-kernel/ApiCore/HttpResults/ApiResult.cs
+++ shared-kernel/ApiCore/HttpResults/ApiResult.cs
@@ -3,16 +3,15 @@ using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PlatformPlatform.SharedKernel.ApplicationCore.Cqrs;
-using IResult = Microsoft.AspNetCore.Http.IResult;
 
 namespace PlatformPlatform.SharedKernel.ApiCore.HttpResults;
 
 public partial class ApiResult : IResult
 {
-    private readonly Result _result;
+    private readonly ResultBase _result;
     private readonly string? _routePrefix;
 
-    public ApiResult(Result result, string? routePrefix = null)
+    protected ApiResult(ResultBase result, string? routePrefix = null)
     {
         _result = result;
         _routePrefix = routePrefix;
@@ -25,7 +24,7 @@ public partial class ApiResult : IResult
             : ConvertResult(_routePrefix).ExecuteAsync(httpContext);
     }
 
-    private IResult ConvertResult(string? routePrefix = null)
+    protected virtual IResult ConvertResult(string? routePrefix = null)
     {
         if (!_result.IsSuccess) return GetProblemDetailsAsJson();
 
@@ -34,7 +33,7 @@ public partial class ApiResult : IResult
             : Results.Created($"{routePrefix}/{_result}", null);
     }
 
-    private IResult GetProblemDetailsAsJson()
+    protected IResult GetProblemDetailsAsJson()
     {
         return Results.Json(CreateProblemDetails(), statusCode: (int) _result.StatusCode);
     }
@@ -70,25 +69,17 @@ public partial class ApiResult : IResult
     }
 }
 
-public partial class ApiResult<T> : IResult
+public class ApiResult<T> : ApiResult
 {
     private readonly Result<T> _result;
-    private readonly string? _routePrefix;
 
     public ApiResult(Result<T> result, string? routePrefix = null)
+        : base(result, routePrefix)
     {
         _result = result;
-        _routePrefix = routePrefix;
     }
 
-    public Task ExecuteAsync(HttpContext httpContext)
-    {
-        return _routePrefix == null
-            ? ConvertResult().ExecuteAsync(httpContext)
-            : ConvertResult(_routePrefix).ExecuteAsync(httpContext);
-    }
-
-    private IResult ConvertResult(string? routePrefix = null)
+    protected override IResult ConvertResult(string? routePrefix = null)
     {
         if (!_result.IsSuccess) return GetProblemDetailsAsJson();
 
@@ -96,36 +87,6 @@ public partial class ApiResult<T> : IResult
             ? Results.Ok(_result.Value!.Adapt<T>())
             : Results.Created($"{routePrefix}/{_result.Value}", null);
     }
-
-    private IResult GetProblemDetailsAsJson()
-    {
-        return Results.Json(CreateProblemDetails(), statusCode: (int) _result.StatusCode);
-    }
-
-    private ProblemDetails CreateProblemDetails()
-    {
-        var statusCode = _result.StatusCode;
-        var problemDetails = new ProblemDetails
-        {
-            Type = $"https://httpstatuses.com/{(int) _result.StatusCode}",
-            Title = SplitCamelCaseTitle(statusCode.ToString()),
-            Status = (int) _result.StatusCode
-        };
-
-        if (_result.ErrorMessage is not null) problemDetails.Detail = _result.ErrorMessage.Message;
-
-        if (_result.Errors?.Length > 0) problemDetails.Extensions[nameof(_result.Errors)] = _result.Errors;
-
-        return problemDetails;
-    }
-
-    private static string SplitCamelCaseTitle(string title)
-    {
-        return SplitCamelCase().Replace(title, " $1");
-    }
-
-    [GeneratedRegex("(?<=[a-z])([A-Z])", RegexOptions.Compiled)]
-    private static partial Regex SplitCamelCase();
 
     public static implicit operator ApiResult<T>(Result<T> result)
     {
