2775f8d9428f1b450ef8dbcb6c4b650b88058df2
Thomas Jespersen <tje@platformplatform.net>
2023-06-11 12:37:16 +0200

    Reset and recreate the database initialization

--- account-management/Infrastructure/Migrations/20230611095743_Initial.Designer.cs
+++ account-management/Infrastructure/Migrations/20230611095743_Initial.Designer.cs
@@ -12,23 +12,52 @@ using PlatformPlatform.AccountManagement.Infrastructure;
 namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(AccountManagementDbContext))]
-    [Migration("20230529195123_Add_Users")]
-    partial class Add_Users
+    [Migration("20230611095743_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.4")
+                .HasAnnotation("ProductVersion", "7.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
+            modelBuilder.Entity("PlatformPlatform.AccountManagement.Domain.Tenants.Tenant", b =>
+                {
+                    b.Property<string>("Id")
+                        .HasColumnType("varchar(30)");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.Property<DateTime?>("ModifiedAt")
+                        .IsConcurrencyToken()
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(30)");
+
+                    b.Property<string>("State")
+                        .IsRequired()
+                        .HasColumnType("varchar(20)");
+
+                    b.Property<string>("Phone")
+                        .HasColumnType("varchar(20)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Tenants");
+                });
+
             modelBuilder.Entity("PlatformPlatform.AccountManagement.Domain.Users.User", b =>
                 {
-                    b.Property<long>("TenantId")
-                        .HasColumnType("bigint");
+                    b.Property<string>("TenantId")
+                        .IsRequired()
+                        .HasColumnType("varchar(30)");
 
                     b.Property<long>("Id")
                         .HasColumnType("bigint");
@@ -46,9 +75,11 @@ namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
 
                     b.Property<string>("UserRole")
                         .IsRequired()
-                        .HasColumnType("nvarchar(20)");
+                        .HasColumnType("varchar(20)");
 
                     b.HasKey("Id");
+
+                    b.HasIndex("TenantId");
 
                     b.ToTable("Users");
                 });
---
+++ account-management/Infrastructure/Migrations/20230611095743_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatformPlatform.AccountManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    [GeneratedCode("Entity Framework", null)]
+    public sealed partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(30)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(30)", nullable: false),
+                    State = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Phone = table.Column<string>(type: "varchar(20)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    TenantId = table.Column<string>(type: "varchar(30)", nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    UserRole = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TenantId",
+                table: "Users",
+                column: "TenantId");
+        }
+    }
+}
