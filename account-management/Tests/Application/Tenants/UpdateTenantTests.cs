ef2fad95f0b42c7796b328841a77809f3566c795
Thomas Jespersen <tje@platformplatform.net>
2023-06-11 17:21:37 +0200

    Create BaseTest class to minimize boilerplate code in test setup. Implement just-in-time construction of ServiceProvider allowing service configuration in tests

--- account-management/Tests/Application/Tenants/UpdateTenantTests.cs
+++ account-management/Tests/Application/Tenants/UpdateTenantTests.cs
@@ -1,33 +1,24 @@
 using FluentAssertions;
-using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
-using PlatformPlatform.AccountManagement.Application;
 using PlatformPlatform.AccountManagement.Application.Tenants;
 using PlatformPlatform.AccountManagement.Domain.Tenants;
+using PlatformPlatform.AccountManagement.Infrastructure;
 using Xunit;
 
 namespace PlatformPlatform.AccountManagement.Tests.Application.Tenants;
 
-public class UpdateTenantTests
+public class UpdateTenantTests : BaseTest<AccountManagementDbContext>
 {
-    private readonly ITenantRepository _tenantRepository;
-
-    public UpdateTenantTests()
-    {
-        var services = new ServiceCollection();
-        services.AddApplicationServices();
-
-        _tenantRepository = Substitute.For<ITenantRepository>();
-    }
-
     [Fact]
     public async Task UpdateTenantHandler_WhenCommandIsValid_ShouldUpdateTenantInRepository()
     {
         // Arrange
         var existingTenant = Tenant.Create("tenant1", "ExistingTenant", "1234567890");
         var existingTenantId = existingTenant.Id;
-        _tenantRepository.GetByIdAsync(existingTenantId, Arg.Any<CancellationToken>()).Returns(existingTenant);
-        var handler = new UpdateTenant.Handler(_tenantRepository);
+
+        var tenantRepository = Substitute.For<ITenantRepository>();
+        tenantRepository.GetByIdAsync(existingTenantId, Arg.Any<CancellationToken>()).Returns(existingTenant);
+        var handler = new UpdateTenant.Handler(tenantRepository);
 
         // Act
         var command = new UpdateTenant.Command {Id = existingTenantId, Name = "UpdatedTenant", Phone = "0987654321"};
@@ -42,8 +33,10 @@ public class UpdateTenantTests
     {
         // Arrange
         var nonExistingTenantId = new TenantId("unknown");
-        _tenantRepository.GetByIdAsync(nonExistingTenantId, Arg.Any<CancellationToken>()).Returns(null as Tenant);
-        var handler = new UpdateTenant.Handler(_tenantRepository);
+
+        var tenantRepository = Substitute.For<ITenantRepository>();
+        tenantRepository.GetByIdAsync(nonExistingTenantId, Arg.Any<CancellationToken>()).Returns(null as Tenant);
+        var handler = new UpdateTenant.Handler(tenantRepository);
 
         // Act
         var command = new UpdateTenant.Command {Id = nonExistingTenantId, Name = "UpdatedTenant", Phone = "0987654321"};
