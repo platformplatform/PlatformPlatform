6e991aa10a852763f121e8696e7e8b327a270719
Thomas Jespersen <tje@platformplatform.net>
2023-06-18 13:33:49 +0200

    Update CreateTenantValidationTests: add scenario parameter and reorder test parameters to match command

--- account-management/Tests/Application/Tenants/CreateTenantValidationTests.cs
+++ account-management/Tests/Application/Tenants/CreateTenantValidationTests.cs
@@ -10,51 +10,49 @@ namespace PlatformPlatform.AccountManagement.Tests.Application.Tenants;
 public sealed class CreateTenantValidationTests : BaseTest<AccountManagementDbContext>
 {
     [Theory]
-    [InlineData("Valid properties", "tenant2", "test@test.com", "+44 (0)20 7946 0123")]
-    [InlineData("No phone number (valid)", "tenant2", "test@test.com", null)]
-    [InlineData("Empty phone number", "tenant2", "test@test.com", "")]
-    public async Task CreateTenant_WhenValidCommand_ShouldReturnSuccessfulResult(string name, string subdomain,
-        string email, string phone)
+    [InlineData("Valid properties", "tenant2", "Tenant 2", "+44 (0)20 7946 0123", "test@test.com")]
+    [InlineData("Valid properties - No phone", "tenant2", "Tenant 2", null, "test@test.com")]
+    [InlineData("Valid properties - Empty phone", "tenant2", "Tenant 2", "", "test@test.com")]
+    public async Task CreateTenant_WhenValidCommand_ShouldReturnSuccessfulResult(string scenario, string subdomain,
+        string name, string phone, string email)
     {
         // Arrange
         var command = new CreateTenant.Command(subdomain, name, phone, email);
-
         var mediator = Provider.GetRequiredService<ISender>();
 
         // Act
         var result = await mediator.Send(command);
 
         // Assert
-        result.IsSuccess.Should().BeTrue();
-        result.Errors.Should().BeNull();
+        result.IsSuccess.Should().BeTrue(scenario);
+        result.Errors.Should().BeNull(scenario);
     }
 
     [Theory]
-    [InlineData("To long phone number", "tenant2", "test@test.com", "0099 (999) 888-77-66-55")]
-    [InlineData("Invalid phone number", "tenant2", "test@test.com", "N/A")]
-    [InlineData("", "notenantname", "test@test.com", "1234567890")]
-    [InlineData("Too long tenant name above 30 characters", "tenant2", "test@test.com", "+55 (21) 99999-9999")]
-    [InlineData("No email", "tenant2", "", "+61 2 1234 5678")]
-    [InlineData("Invalid Email", "tenant2", "@test.com", "1234567890")]
-    [InlineData("No subdomain", "", "test@test.com", "1234567890")]
-    [InlineData("To short subdomain", "ab", "test@test.com", "1234567890")]
-    [InlineData("To long subdomain", "1234567890123456789012345678901", "test@test.com", "1234567890")]
-    [InlineData("Subdomain with uppercase", "Tenant1", "test@test.com", "1234567890")]
-    [InlineData("Subdomain special characters", "tenant-1", "test@test.com", "1234567890")]
-    [InlineData("Subdomain with spaces", "tenant 1", "test@test.com", "1234567890")]
-    public async Task CreateTenant_WhenInvalidCommand_ShouldReturnUnsuccessfulResultWithOneError(string name,
-        string subdomain, string email, string phone)
+    [InlineData("Phone number too long", "tenant2", "Tenant 2", "123456789012345678901", "test@test.com")]
+    [InlineData("Phone number invalid ", "tenant2", "Tenant 2", "+1 ### ###-INVALID", "test@test.com")]
+    [InlineData("Tenant name empty", "", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Tenant name too long ", "tenant2", "1234567890123456789012345678901", "1234567890", "test@test.com")]
+    [InlineData("Email empty", "tenant2", "Tenant 2", "1234567890", "")]
+    [InlineData("Email invalid", "tenant2", "Tenant 2", "1234567890", "@test.com")]
+    [InlineData("Subdomain empty", "", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Subdomain too short", "12", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Subdomain too long", "1234567890123456789012345678901", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Subdomain with uppercase", "Tenant2", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Subdomain special characters", "tenant-2", "Tenant 2", "1234567890", "test@test.com")]
+    [InlineData("Subdomain with spaces", "tenant 2", "Tenant 2", "1234567890", "test@test.com")]
+    public async Task CreateTenant_WhenInvalidCommand_ShouldReturnUnsuccessfulResultWithOneError(string scenario,
+        string subdomain, string name, string phone, string email)
     {
         // Arrange
         var command = new CreateTenant.Command(subdomain, name, phone, email);
-
         var mediator = Provider.GetRequiredService<ISender>();
 
         // Act
         var result = await mediator.Send(command);
 
         // Assert
-        result.IsSuccess.Should().BeFalse();
-        result.Errors?.Length.Should().Be(1);
+        result.IsSuccess.Should().BeFalse(scenario);
+        result.Errors?.Length.Should().Be(1, scenario);
     }
 }
