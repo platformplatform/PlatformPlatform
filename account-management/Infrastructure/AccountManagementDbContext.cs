d01ad67a362425ca8545858eff32867051cb5f18
Thomas Jespersen <tje@platformplatform.net>
2023-06-18 15:31:46 +0200

    Change mapping of UserId to use new MapStronglyTypedUuid for database mapping with Entity Framework

--- account-management/Infrastructure/AccountManagementDbContext.cs
+++ account-management/Infrastructure/AccountManagementDbContext.cs
@@ -23,7 +23,7 @@ public sealed class AccountManagementDbContext : SharedKernelDbContext<AccountMa
         modelBuilder.MapStronglyTypedId<Tenant, TenantId, string>(t => t.Id);
 
         // User
-        modelBuilder.MapStronglyTypedId<User, UserId>(u => u.Id);
+        modelBuilder.MapStronglyTypedUuid<User, UserId>(u => u.Id);
         modelBuilder.MapStronglyTypedId<User, TenantId, string>(u => u.TenantId);
         modelBuilder.Entity<User>().HasOne<Tenant>().WithMany().HasForeignKey(u => u.TenantId)
             .HasPrincipalKey(t => t.Id);
