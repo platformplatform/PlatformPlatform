e852497bc9cb473e79ac9822f54edb29b8a5c294
Thomas Jespersen <tje@platformplatform.net>
2023-08-20 17:57:24 +0200

    Show error message for adding SQL Server to built-in "Directory Readers" Azure AD Group if granting permissions fails

--- cloud-infrastructure/cluster/grant-database-permissions.sh
+++ cloud-infrastructure/cluster/grant-database-permissions.sh
@@ -6,10 +6,10 @@ SQL_SERVER="$SQL_SERVER_NAME.database.windows.net"
 cd "$(dirname "${BASH_SOURCE[0]}")"
 . ./firewall.sh open
 
-echo Granting $MANAGED_IDENTITY permissions on $SQL_SERVER/$SQL_DATABASE database
+echo Granting $MANAGED_IDENTITY in Recource group $RESOURCE_GROUP_NAME permissions on $SQL_SERVER/$SQL_DATABASE database
 
 # Execute the SQL script using mssql-scripter. Pass the script as a heredoc to sqlcmd to allow for complex SQL.
-sqlcmd -S $SQL_SERVER -d $SQL_DATABASE --authentication-method=ActiveDirectoryDefault << EOF
+sqlcmd -S $SQL_SERVER -d $SQL_DATABASE --authentication-method=ActiveDirectoryDefault --exit-on-error << EOF
 IF NOT EXISTS (SELECT [name] FROM [sys].[database_principals] WHERE [name] = '$MANAGED_IDENTITY' AND [type] = 'E')
 BEGIN
     CREATE USER [$MANAGED_IDENTITY] FROM EXTERNAL PROVIDER;
@@ -19,5 +19,12 @@ BEGIN
 END
 GO
 EOF
+
+# Check the exit status of the sqlcmd command
+if [ $? -eq 0 ]; then
+  echo "Permissions granted successfully"
+else
+  echo "Please add the $SQL_SERVER_NAME to the Azure AD built-in \"Directory Readers\" group and try again."
+fi
 
 . ./firewall.sh close
