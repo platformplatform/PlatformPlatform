b53497a7a41e9d5b02adb7dcdf21cca9be1e4c86
Thomas Jespersen <tje@platformplatform.net>
2023-09-10 21:23:53 +0200

    Delete testing scripts for shared, environment, and cluster

--- cloud-infrastructure/cluster/config/testing-west-europe.sh
+++
@@ -1,8 +0,0 @@
-ENVIRONMENT="testing"
-LOCATION="WestEurope"
-LOCATION_PREFIX="west-europe"
-CLUSTER_UNIQUE_NAME="${UNIQUE_CLUSTER_PREFIX}testweu"
-USE_MSSQL_ELASTIC_POOL=false
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-. ../deploy-cluster.sh
