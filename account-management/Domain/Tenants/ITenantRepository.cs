ea63966522d0751695a3462c0d85e7214e07f6ba
Thomas Jespersen <tje@platformplatform.net>
2023-09-19 20:48:18 +0200

    Delete generic IRepository and allow ITenantRepository and IUserRepository to specify their own interfaces, facilitating ReadOnly repository options

--- account-management/Domain/Tenants/ITenantRepository.cs
+++ account-management/Domain/Tenants/ITenantRepository.cs
@@ -1,8 +1,16 @@
-using PlatformPlatform.SharedKernel.DomainCore.Persistence;
-
 namespace PlatformPlatform.AccountManagement.Domain.Tenants;
 
-public interface ITenantRepository : IRepository<Tenant, TenantId>
+public interface ITenantRepository
 {
+    Task<Tenant?> GetByIdAsync(TenantId id, CancellationToken cancellationToken);
+
+    Task<bool> ExistsAsync(TenantId id, CancellationToken cancellationToken);
+
+    Task AddAsync(Tenant aggregate, CancellationToken cancellationToken);
+
+    void Update(Tenant aggregate);
+
+    void Remove(Tenant aggregate);
+
     Task<bool> IsSubdomainFreeAsync(string subdomain, CancellationToken cancellationToken);
 }
--- account-management/Domain/Users/IUserRepository.cs
+++ account-management/Domain/Users/IUserRepository.cs
@@ -1,9 +1,15 @@
-using PlatformPlatform.SharedKernel.DomainCore.Persistence;
-
 namespace PlatformPlatform.AccountManagement.Domain.Users;
 
-public interface IUserRepository : IRepository<User, UserId>
+public interface IUserRepository
 {
+    Task<User?> GetByIdAsync(UserId id, CancellationToken cancellationToken);
+
+    Task AddAsync(User aggregate, CancellationToken cancellationToken);
+
+    void Update(User aggregate);
+
+    void Remove(User aggregate);
+
     Task<bool> IsEmailFreeAsync(TenantId tenantId, string email, CancellationToken cancellationToken);
 
     Task<int> CountTenantUsersAsync(TenantId tenantId, CancellationToken cancellationToken);
