cfc9deef48756260db2a1bcf8986ea139bcaa809
Thomas Jespersen <tje@platformplatform.net>
2023-10-31 21:00:59 +0100

    Use non-null assertion operator for root element and omit file extension in App import

--- application/account-management/WebApp/src/main.tsx
+++ application/account-management/WebApp/src/main.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement == null) {
-  throw new Error("Could not find root element");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
