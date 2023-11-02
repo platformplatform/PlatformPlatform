f05e78d13a4038534e078be742610378526a759a
Thomas Jespersen <tje@platformplatform.net>
2023-11-02 12:26:47 +0100

    Ensure Tailwind styling is applied to index.html
    Co-authored-by: Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
    Signed-off-by: Thomas Jespersen <tje@platformplatform.net>

--- application/account-management/WebApp/tailwind.config.js
+++ application/account-management/WebApp/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}", "./public/index.html"],
   darkMode: ["class"],
   theme: {
     container: {
