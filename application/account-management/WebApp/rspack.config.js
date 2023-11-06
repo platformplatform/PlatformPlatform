bb3e8311de9f1daee1868e96121a9f993b8be4b0
raix <1136718+raix@users.noreply.github.com>
2023-11-06 12:23:31 +0100

    Setup openApi fetch, RAC form validation using useFormState and generate types in Api

--- application/account-management/WebApp/rspack.config.js
+++ application/account-management/WebApp/rspack.config.js
@@ -1,10 +1,15 @@
+const path = require("path");
+
 /**
- * @type {import('@rspack/cli').Configuration}
+ * @type {import("@rspack/cli").Configuration}
  */
 module.exports = {
   context: __dirname,
   entry: {
     main: "./src/main.tsx",
+  },
+  resolve: {
+    tsConfigPath: path.resolve(__dirname, "tsconfig.json"),
   },
   builtins: {
     html: [
