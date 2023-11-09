467c2539264f0d80640b11a208030b0472c62024
Thomas Jespersen <tje@platformplatform.net>
2023-11-09 16:35:55 +0100

    Print Azure Bicep deployment commands to console for improved debugging

--- cloud-infrastructure/deploy.sh
+++ cloud-infrastructure/deploy.sh
@@ -1,4 +1,6 @@
 set -eo pipefail
+CYAN='\033[0;36m'
+RESET='\033[0m' # Reset formatting
 
 if [[ "$1" == "" ]]
 then
@@ -9,6 +11,7 @@ fi
 if [[ "$*" == *"--plan"* ]]
 then
     echo "$(date +"%Y-%m-%dT%H:%M:%S") Preparing plan..."
+    echo -e "$CYAN$DEPLOYMENT_COMMAND -w $DEPLOYMENT_PARAMETERS$RESET"
     $DEPLOYMENT_COMMAND -w $DEPLOYMENT_PARAMETERS
     if [[ $? -ne 0 ]]; then
         echo "::error::Plan preparation failed."
@@ -19,5 +22,6 @@ fi
 if [[ "$*" == *"--apply"* ]]
 then
     echo "$(date +"%Y-%m-%dT%H:%M:%S") Applying changes..."
+    echo -e "$CYAN$DEPLOYMENT_COMMAND $DEPLOYMENT_PARAMETERS$RESET"
     export output=$($DEPLOYMENT_COMMAND $DEPLOYMENT_PARAMETERS 2>&1)
 fi
