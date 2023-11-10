6d25b18b2e4b76d49f70dbfd0839f0ccf6d43e8e
raix <1136718+raix@users.noreply.github.com>
2023-11-10 22:17:20 +0100

    Rename generated file for clarity and origin

--- application/account-management/WebApp/src/lib/api/client.ts
+++ application/account-management/WebApp/src/lib/api/client.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch";
-import type { paths } from "./schema";
+import type { paths } from "./api.generated";
 
 const baseUrl = "https://localhost:8443";
 export const accountManagementApi = createClient<paths>({ baseUrl });
