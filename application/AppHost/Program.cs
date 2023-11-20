e7829191436503d88e36475b8b706683fe41835a
Thomas Jespersen <tje@platformplatform.net>
2023-11-20 17:17:26 +0100

    Setup Azure SQL Edge container in Aspire

--- application/AppHost/Program.cs
+++ application/AppHost/Program.cs
@@ -3,6 +3,15 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sqlPassword = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD")
+                  ?? throw new InvalidOperationException("Missing SQL_SERVER_PASSWORD environment variable.");
+
+builder.AddContainer("localhost", "mcr.microsoft.com/azure-sql-edge")
+    .WithEnvironment("ACCEPT_EULA", "Y")
+    .WithEnvironment("SA_PASSWORD", sqlPassword)
+    .WithServiceBinding(1433, "tcp", "localhost")
+    .WithVolumeMount("sql-server-data", "/var/opt/mssql", VolumeMountType.Named);
+
 var accountManagementApi = builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api");
 
 builder.AddBunApp("frontend", "../account-management/WebApp", "dev")
