3c65e65076eaf58fda58af8a946b0dbb2917f84a
Thomas Jespersen <tje@platformplatform.net>
2023-11-20 19:41:39 +0100

    Temporarily disable starting Azure SQL Edge container with Aspire, use docker-compose instead

--- application/AppHost/Program.cs
+++ application/AppHost/Program.cs
@@ -1,16 +1,18 @@
-﻿using AppHost;
+﻿using System.Diagnostics;
+using AppHost;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
+// var sqlPassword = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD")
+//                   ?? throw new InvalidOperationException("Missing SQL_SERVER_PASSWORD environment variable.");
+//
+// builder.AddContainer("localhost", "mcr.microsoft.com/azure-sql-edge")
+//     .WithEnvironment("ACCEPT_EULA", "Y")
+//     .WithEnvironment("SA_PASSWORD", sqlPassword)
+//     .WithServiceBinding(1433, "tcp", "localhost")
+//     .WithVolumeMount("sql-server-data", "/var/opt/mssql", VolumeMountType.Named);
 
-var sqlPassword = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD")
-                  ?? throw new InvalidOperationException("Missing SQL_SERVER_PASSWORD environment variable.");
-
-builder.AddContainer("localhost", "mcr.microsoft.com/azure-sql-edge")
-    .WithEnvironment("ACCEPT_EULA", "Y")
-    .WithEnvironment("SA_PASSWORD", sqlPassword)
-    .WithServiceBinding(1433, "tcp", "localhost")
-    .WithVolumeMount("sql-server-data", "/var/opt/mssql", VolumeMountType.Named);
+StartSqlServerUsingDockerCompose();
 
 var accountManagementApi = builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api");
 
@@ -18,3 +20,21 @@ builder.AddBunApp("frontend", "../account-management/WebApp", "dev")
     .WithReference(accountManagementApi);
 
 builder.Build().Run();
+
+// Temporary workaround until staring container using builder.AddContainer() is working.
+static void StartSqlServerUsingDockerCompose()
+{
+    try
+    {
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo { FileName = "docker", Arguments = "compose up sql-server -d" };
+        process.Start();
+        process.WaitForExit();
+        Thread.Sleep(TimeSpan.FromSeconds(3)); // Ensure SQL Server is ready
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+        throw;
+    }
+}
