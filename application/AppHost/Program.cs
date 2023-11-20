c87b2384b6ce06435e925ba656a2223a79c0d8b8
Thomas Jespersen <tje@platformplatform.net>
2023-11-20 16:02:12 +0100

    Configure Aspire to host React SPA with Bun

--- application/AppHost/Program.cs
+++ application/AppHost/Program.cs
@@ -1,7 +1,11 @@
-﻿using Projects;
+﻿using AppHost;
+using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api");
+var accountManagementApi = builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api");
+
+builder.AddBunApp("frontend", "../account-management/WebApp", "dev")
+    .WithReference(accountManagementApi);
 
 builder.Build().Run();
