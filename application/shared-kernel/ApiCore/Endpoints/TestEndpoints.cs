43a0bf9266b2af24b4420e8e1c8c474712c158d7
raix <1136718+raix@users.noreply.github.com>
2023-11-22 09:36:48 +0100

    Mount test endpoint on /api path

--- application/shared-kernel/ApiCore/Endpoints/TestEndpoints.cs
+++ application/shared-kernel/ApiCore/Endpoints/TestEndpoints.cs
@@ -10,6 +10,6 @@ public static class TestEndpoints
         if (!bool.TryParse(Environment.GetEnvironmentVariable("TestEndpointsEnabled"), out _)) return;
 
         // Add a dummy endpoint that throws an InvalidOperationException for testing purposes.
-        routes.MapGet("/throwException", _ => throw new InvalidOperationException("Dummy endpoint for testing."));
+        routes.MapGet("/api/throwException", _ => throw new InvalidOperationException("Dummy endpoint for testing."));
     }
 }
