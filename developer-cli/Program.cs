6a0a396681aa404b3591b5b13adb3917e0c89370
Thomas Jespersen <tje@platformplatform.net>
2023-12-10 16:59:02 +0100

    Add intro in CLI tool for help and exit with error code on unknown command

--- developer-cli/Program.cs
+++ developer-cli/Program.cs
@@ -5,4 +5,31 @@ ChangeDetection.EnsureCliIsCompiledWithLatestChanges(args);
 AliasRegistration.EnsureAliasIsRegistered();
 
 var command = args.FirstOrDefault();
-AnsiConsole.MarkupLine($"Unknown command [green]{command}[/]. Use [green]--help[/] to see available commands.");
+
+switch (command)
+{
+    case null:
+    case "--help":
+    case "-h":
+        AnsiConsole.MarkupLine(
+            """
+            [green]Welcome to the PlatformPlatform CLI:[/]
+
+            Here are the base commands:
+            
+               [yellow]No commands implemented yet![/]
+
+            Use `--version` to display the current version.
+            """);
+        break;
+
+    default:
+        AnsiConsole.MarkupLine(
+            $"""
+             [red]'{command}' is misspelled or an unknown command[/].
+
+             Please use [green]-help[/] to see the list of available commands.
+             """);
+        Environment.ExitCode = 1;
+        break;
+}
