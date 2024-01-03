e5a9300c5d56397a05e111c912d436179e721992
Thomas Jespersen <tje@platformplatform.net>
2024-01-03 11:25:34 +0100

    Add step to login to Azure and show and select Azure Subscriptions

--- developer-cli/Commands/InitializeGitHubAndAzureWorkflow.cs
+++ developer-cli/Commands/InitializeGitHubAndAzureWorkflow.cs
@@ -1,25 +1,37 @@
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
-using Azure.Core;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
+using PlatformPlatform.DeveloperCli.Utilities;
 using Spectre.Console;
+using Environment = System.Environment;
 
 namespace PlatformPlatform.DeveloperCli.Commands;
 
 [UsedImplicitly]
 public class InitializeGitHubAndAzureWorkflow : Command
 {
+    private static readonly JsonSerializerOptions? JsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
+
     public InitializeGitHubAndAzureWorkflow() : base(
         "initialize-github-and-azure-workflow",
         "Set up trust between Azure and GitHub for passwordless deployments using Azure Service Principals with OpenID (aka. Federated Credentials)."
     )
     {
-        Handler = CommandHandler.Create(Execute);
+        var skipAzureLoginOption = new Option<bool>(["--skip-azure-login"], "Skip Azure login");
+
+        AddOption(skipAzureLoginOption);
+
+        Handler = CommandHandler.Create<bool>(Execute);
     }
 
-    private int Execute()
+    private int Execute(bool skipAzureLogin = false)
     {
-        // Prompting for Azure Subscription
+        var subscription = GetAzureSubscription(skipAzureLogin);
+
+        AnsiConsole.WriteLine(
+            $"Azure subscription {subscription.Name} ({subscription.Id}) on Tenant {subscription.TenantId} selected.");
 
         // Prompting GitHub Repository
 
@@ -35,4 +47,45 @@ public class InitializeGitHubAndAzureWorkflow : Command
 
         return 0;
     }
+
+    private Subscription GetAzureSubscription(bool skipAzureLogin)
+    {
+        // Both `az login` and `az account list` will return a JSON array of subscriptions
+        var accountList = skipAzureLogin ? "account list" : "login";
+        var subscriptionListJson = ProcessHelper.StartProcess("az", accountList, redirectOutput: true);
+
+        // Regular expression to match JSON part
+        var jsonRegex = new Regex(@"\[.*\]", RegexOptions.Singleline);
+        var match = jsonRegex.Match(subscriptionListJson);
+
+        List<Subscription>? azureSubscriptions = null;
+        if (match.Success)
+        {
+            azureSubscriptions = JsonSerializer.Deserialize<List<Subscription>>(match.Value, JsonSerializerOptions);
+        }
+
+        if (azureSubscriptions == null)
+        {
+            AnsiConsole.MarkupLine("[red]ERROR:[/] No subscriptions found.");
+            Environment.Exit(1);
+        }
+
+        var activeSubscriptions = azureSubscriptions.Where(s => s.State == "Enabled").ToList();
+
+        var selectedDisplayName = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("Please select an Azure subscription")
+            .AddChoices(activeSubscriptions.Select(s => s.Name)));
+
+        var selectedSubscriptions = activeSubscriptions.Where(s => s.Name == selectedDisplayName).ToArray();
+        if (selectedSubscriptions.Length > 1)
+        {
+            AnsiConsole.MarkupLine($"[red]ERROR:[/] Found two subscription with the name {selectedDisplayName}.");
+            Environment.Exit(1);
+        }
+
+        return selectedSubscriptions.Single();
+    }
 }
+
+[UsedImplicitly]
+public record Subscription(string Id, string Name, string TenantId, string State);
