21deaa3eb3402191773eeb17d1efed07ac412dd1
Thomas Jespersen <tje@platformplatform.net>
2024-01-07 19:25:05 +0100

    Add standard input option in ProcessHelper.StartProcess

--- developer-cli/Utilities/ProcessHelper.cs
+++ developer-cli/Utilities/ProcessHelper.cs
@@ -7,6 +7,7 @@ public static class ProcessHelper
 {
     public static string StartProcess(
         ProcessStartInfo processStartInfo,
+        string? input = null,
         bool waitForExit = true,
         bool printCommand = true
     )
@@ -18,13 +19,17 @@ public static class ProcessHelper
         }
 
         var process = Process.Start(processStartInfo)!;
-
-        if (!waitForExit) return string.Empty;
+        if (input is not null)
+        {
+            process.StandardInput.WriteLine(input);
+            process.StandardInput.Close();
+        }
 
         var output = string.Empty;
         if (processStartInfo.RedirectStandardOutput) output += process.StandardOutput.ReadToEnd();
         if (processStartInfo.RedirectStandardError) output += process.StandardError.ReadToEnd();
 
+        if (!waitForExit) return string.Empty;
         process.WaitForExit();
 
         return output;
