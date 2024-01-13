804119d4b1f247aa488079f3abd9e30cfaf1ac77
Thomas Jespersen <tje@platformplatform.net>
2024-01-13 10:10:04 +0100

    Use Aspire's built-in AddSqlServerContainer and switch from retired Azure SQL Edge to SQL Server

--- application/AppHost/Program.cs
+++ application/AppHost/Program.cs
@@ -1,38 +1,17 @@
-﻿using System.Diagnostics;
 using AppHost;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-StartSqlServerUsingDockerCompose();
+var sqlServerPassword = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
+var database = builder.AddSqlServerContainer("Default", sqlServerPassword, 1433)
+    .WithVolumeMount("sql-server-data", "/var/opt/mssql", VolumeMountType.Named)
+    .AddDatabase("account-management");
 
-var accountManagementApi = builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api");
+var accountManagementApi = builder.AddProject<PlatformPlatform_AccountManagement_Api>("account-management-api")
+    .WithReference(database);
 
 builder.AddBunApp("frontend", "../account-management/WebApp", "dev")
     .WithReference(accountManagementApi);
 
 builder.Build().Run();
-
-return;
-
-// Temporary workaround until staring SQL Server using Aspire is fixed: https://github.com/dotnet/aspire/issues/1023
-static void StartSqlServerUsingDockerCompose()
-{
-    try
-    {
-        using var process = new Process();
-
-#pragma warning disable S4036 // Disable "Searching OS commands in PATH is security-sensitive"
-        process.StartInfo = new ProcessStartInfo { FileName = "docker", Arguments = "compose up sql-server -d" };
-#pragma warning restore S4036
-
-        process.Start();
-        process.WaitForExit();
-        Thread.Sleep(TimeSpan.FromSeconds(3)); // Ensure SQL Server is ready
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"An error occurred: {ex.Message}");
-        throw;
-    }
-}
