fbf1519ce8d8d0df25398ee89b4c4564cf89766e
Thomas Jespersen <tje@platformplatform.net>
2024-01-17 13:56:07 +0100

    Open Aspire Dashboard and AccountManagement SPA when the CLI command has started the hosts

--- developer-cli/Commands/RunCommand.cs
+++ developer-cli/Commands/RunCommand.cs
@@ -1,5 +1,6 @@
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
+using System.Diagnostics;
 using JetBrains.Annotations;
 using PlatformPlatform.DeveloperCli.Installation;
 using PlatformPlatform.DeveloperCli.Utilities;
@@ -14,12 +15,46 @@ public class RunCommand : Command
         Handler = CommandHandler.Create(Execute);
     }
 
-    private int Execute()
+    private void Execute()
     {
         var workingDirectory = Path.Combine(Configuration.GetSourceCodeFolder(), "..", "application", "AppHost");
 
-        ProcessHelper.StartProcess($"dotnet run {workingDirectory}", workingDirectory);
+        Task.Run(async () =>
+        {
+            // Start a background task that monitors the websites and opens the browser when ready
+            const int aspireDashboardPort = 8001;
+            await StartBrowserWhenSiteIsReady(aspireDashboardPort);
+            const int accountManagementPort = 8443;
+            await StartBrowserWhenSiteIsReady(accountManagementPort);
+        });
 
-        return 0;
+        ProcessHelper.StartProcess("dotnet run", workingDirectory);
+    }
+
+    private static async Task StartBrowserWhenSiteIsReady(int port)
+    {
+        var url = $"https://localhost:{port}";
+
+        var httpClient = new HttpClient { Timeout = TimeSpan.FromSeconds(10) };
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("PlatformPlatform Developer CLI");
+
+        while (true)
+        {
+            try
+            {
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    break;
+                }
+            }
+            catch (HttpRequestException) // DNS issues, server down etc.
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        ProcessHelper.StartProcess(new ProcessStartInfo("open", url), waitForExit: false);
     }
 }
