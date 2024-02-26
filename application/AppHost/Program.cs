8167d2b629d7968fac4b809b857a0b1f8cb77e71
Maria Stypsanelli <mstypsan.src@gmail.com>
2024-02-26 12:19:07 +0100

    Specify a specific port for the SQL server container

--- application/AppHost/Program.cs
+++ application/AppHost/Program.cs
@@ -3,7 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var sqlServerPassword = Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
-var database = builder.AddSqlServerContainer("account-management-db", sqlServerPassword)
+var database = builder.AddSqlServerContainer("account-management-db", sqlServerPassword, 8433)
     .WithVolumeMount("sql-server-data", "/var/opt/mssql", VolumeMountType.Named)
     .AddDatabase("account-management");
 
