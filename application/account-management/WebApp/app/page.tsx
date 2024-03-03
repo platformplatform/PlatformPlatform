627fc2265314eff54bf48495b603f2bc1a047ccf
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-03-03 22:52:21 +0100

    Convert images to webp and create HeroImage component

--- application/account-management/WebApp/app/page.tsx
+++ application/account-management/WebApp/app/page.tsx
@@ -1,12 +1,9 @@
 import { Trans } from "@lingui/macro";
-import { useLingui } from "@lingui/react";
-import heroDesktop from "./hero-desktop.png";
-import heroMobile from "./hero-mobile.png";
 import { Link } from "@/lib/router/router";
 import AcmeLogo from "@/ui/AcmeLogo";
+import { HeroImage } from "@/ui/images/HeroImage";
 
 export default function LandingPage() {
-  const { i18n } = useLingui();
   return (
     <main className="flex min-h-screen flex-col">
       <div className="flex h-20 shrink-0 items-end bg-black dark:bg-white p-4 md:h-52">
@@ -34,20 +31,7 @@ export default function LandingPage() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 bg-gray-50 md:w-3/5 md:px-28 md:py-12">
-          <img
-            src={heroMobile}
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt={i18n.t("Screenshots of the dashboard project showing mobile versions")}
-          />
-          <img
-            src={heroDesktop}
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt={i18n.t("Screenshots of the dashboard project showing desktop and mobile versions")}
-          />
+          <HeroImage />
         </div>
       </div>
     </main>
--- application/account-management/WebApp/app/register/page.tsx
+++ application/account-management/WebApp/app/register/page.tsx
@@ -1,11 +1,7 @@
-import { useLingui } from "@lingui/react";
-import heroDesktop from "@/app/hero-desktop.png";
-import heroMobile from "@/app/hero-mobile.png";
 import { SignUpForm } from "@/ui/Auth/SignUpForm";
+import { HeroImage } from "@/ui/images/HeroImage";
 
 export default function SignUpPage() {
-  const { i18n } = useLingui();
-
   return (
     <main className="flex min-h-screen flex-col">
       <div className="flex grow flex-col gap-4 md:flex-row">
@@ -14,20 +10,7 @@ export default function SignUpPage() {
           <SignUpForm />
         </div>
         <div className="flex items-center justify-center p-6 bg-gray-50 md:w-3/5 md:px-28 md:py-12">
-          <img
-            src={heroMobile}
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt={i18n.t("Screenshots of the dashboard project showing mobile versions")}
-          />
-          <img
-            src={heroDesktop}
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt={i18n.t("Screenshots of the dashboard project showing desktop and mobile versions")}
-          />
+          <HeroImage />
         </div>
       </div>
     </main>
