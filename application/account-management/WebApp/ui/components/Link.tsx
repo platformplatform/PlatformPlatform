97433e1d22faa4c0db7f1f88e75e9772f8b4b4ae
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-03-04 14:39:58 +0100

    Configure React Aria Components link

--- application/account-management/WebApp/ui/components/Link.tsx
+++ application/account-management/WebApp/ui/components/Link.tsx
@@ -14,10 +14,11 @@ const styles = tv({
     variant: {
       primary: "text-blue-600 dark:text-blue-500 underline decoration-blue-600/60 hover:decoration-blue-600 dark:decoration-blue-500/60 dark:hover:decoration-blue-500",
       secondary: "text-gray-700 dark:text-zinc-300 underline decoration-gray-700/50 hover:decoration-gray-700 dark:decoration-zinc-300/70 dark:hover:decoration-zinc-300",
+      neutral: "text-neutral-600 dark:text-neutral-500 underline decoration-neutral-600/60 hover:decoration-neutral-600 dark:decoration-neutral-500/60 dark:hover:decoration-neutral-500",
     },
   },
   defaultVariants: {
-    variant: "primary",
+    variant: "neutral",
   },
 });
 
