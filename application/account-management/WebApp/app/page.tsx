103858c35a7490bfd2e471f44906d2b14abf29b1
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-03-04 14:54:46 +0100

    Landing page use Button instead of Link

--- application/account-management/WebApp/app/page.tsx
+++ application/account-management/WebApp/app/page.tsx
@@ -1,9 +1,11 @@
 import { Trans } from "@lingui/macro";
-import { Link } from "@/lib/router/router";
+import { useNavigate } from "@/lib/router/router";
 import AcmeLogo from "@/ui/AcmeLogo";
+import { Button } from "@/ui/components/Button";
 import { HeroImage } from "@/ui/images/HeroImage";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
   return (
     <main className="flex min-h-screen flex-col">
       <div className="flex h-20 shrink-0 items-end bg-black dark:bg-white p-4 md:h-52">
@@ -12,7 +14,7 @@ export default function LandingPage() {
       <div className="flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 md:w-2/5 md:px-20 p-6">
           <p
-            className="text-xl text-neutral-800 md:text-3xl md:leading-normal"
+            className="text-xl text-neutral-800 md:text-2xl md:leading-normal"
           >
             <strong><Trans>Welcome to Acme.</Trans></strong> <Trans>This is the example for the
               {" "}
@@ -23,12 +25,9 @@ export default function LandingPage() {
               demo product, brought to you by PlatformPlatform
             </Trans>
           </p>
-          <Link
-            to="/login"
-            className="flex items-center gap-5 self-start rounded bg-black px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-neutral-800 md:text-base"
-          >
+          <Button onPress={() => navigate("/login")} className="w-fit">
             <Trans>Sign in</Trans>
-          </Link>
+          </Button>
         </div>
         <div className="flex items-center justify-center p-6 bg-gray-50 md:w-3/5 md:px-28 md:py-12">
           <HeroImage />
