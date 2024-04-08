53d6396041489932f77f2a7ab944b179ee5ea731
Maria Stypsanelli <mstypsan.src@gmail.com>
2024-04-08 10:46:19 +0200

    Publish the API using dotnet publish and start using the default 8080 port

--- application/shared-kernel/ApiCore/ApiCoreConfiguration.cs
+++ application/shared-kernel/ApiCore/ApiCoreConfiguration.cs
@@ -118,11 +118,7 @@ public static class ApiCoreConfiguration
         else
         {
             // When running inside a Docker container running as non-root we need to use a port higher than 1024
-            builder.WebHost.ConfigureKestrel(options =>
-            {
-                options.ListenAnyIP(8443, _ => { });
-                options.AddServerHeader = false;
-            });
+            builder.WebHost.ConfigureKestrel(options => { options.AddServerHeader = false; });
         }
 
         return services;
