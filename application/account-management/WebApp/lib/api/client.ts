d15dd3513349cf2c237c97aa60c6e1cfd3913a7f
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-04-12 00:40:05 +0200

    Fix Api url for now

--- application/account-management/WebApp/lib/api/client.ts
+++ application/account-management/WebApp/lib/api/client.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./api.generated";
 
-const baseUrl = import.meta.env.PUBLIC_URL;
+const baseUrl = "/api/account-management";
 export const accountManagementApi = createClient<paths>({ baseUrl });
