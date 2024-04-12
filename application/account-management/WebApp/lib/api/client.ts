db6991b8ad25de0d3ecf135cacde4a0e7b2a6e38
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-04-12 02:08:03 +0200

    Remove usage of baseUrl for api client

--- application/account-management/WebApp/lib/api/client.ts
+++ application/account-management/WebApp/lib/api/client.ts
@@ -1,5 +1,4 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./api.generated";
 
-const baseUrl = "";
-export const accountManagementApi = createClient<paths>({ baseUrl });
+export const accountManagementApi = createClient<paths>();
