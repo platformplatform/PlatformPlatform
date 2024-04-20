d7807b16285856ec18d3f7b9c6bd18c03c7d4aea
Thomas Jespersen <tje@platformplatform.net>
2024-04-20 10:38:27 +0200

    Configure dependency injection in Workers project

--- application/account-management/Workers/Program.cs
+++ application/account-management/Workers/Program.cs
@@ -1,4 +1,14 @@
+using PlatformPlatform.AccountManagement.Application;
+using PlatformPlatform.AccountManagement.Infrastructure;
+
 var builder = Host.CreateApplicationBuilder(args);
+
+// Configure services for the Application, Infrastructure layers like Entity Framework, Repositories, MediatR,
+// FluentValidation validators, Pipelines.
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices()
+    .ConfigureStorage(builder);
 
 var host = builder.Build();
 host.Run();
