e6e82a4ff173f00fb9a64dabf9accbce2c15d94d
Thomas Jespersen <tje@platformplatform.net>
2024-04-30 14:54:11 +0200

    Add proxy config filter to replace localhost cluster destination addresses with environment variables when running in Azure

--- application/AppGateway/Program.cs
+++ application/AppGateway/Program.cs
@@ -1,8 +1,11 @@
+using PlatformPlatform.AppGateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddConfigFilter<ClusterDestinationConfigFilter>();
 
 var app = builder.Build();
 
