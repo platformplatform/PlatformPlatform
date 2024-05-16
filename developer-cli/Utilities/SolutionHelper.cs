89d913e62f238c1a664c6c9cff18fc8a44deb95f
Thomas Jespersen <tje@platformplatform.net>
2024-05-16 19:05:02 +0200

    Update CLI to prompt for solution file only when multiple solution files are present

---
+++ developer-cli/Utilities/SolutionHelper.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using PlatformPlatform.DeveloperCli.Installation;
+using Spectre.Console;
+
+namespace PlatformPlatform.DeveloperCli.Utilities;
+
+public class SolutionHelper(string imageName, string instanceName, int? port, string? volume)
+{
+    public static FileInfo GetSolution(string? solutionName)
+    {
+        var workingDirectory = Path.Combine(Configuration.GetSourceCodeFolder(), "..", "application");
+
+        var solutionsFiles = Directory
+            .GetFiles(workingDirectory, "*.sln", SearchOption.AllDirectories)
+            .ToDictionary(s => new FileInfo(s).Name.Replace(".sln", ""), s => s);
+
+        if (solutionName is not null && !solutionsFiles.ContainsKey(solutionName))
+        {
+            AnsiConsole.MarkupLine($"[red]ERROR:[/] Solution [yellow]{solutionName}[/] not found.");
+            Environment.Exit(1);
+        }
+
+        if (solutionsFiles.Count == 1)
+        {
+            solutionName = solutionsFiles.Keys.Single();
+        }
+
+        if (solutionName is null)
+        {
+            var prompt = new SelectionPrompt<string>()
+                .Title("Please select an option")
+                .AddChoices(solutionsFiles.Keys);
+
+            solutionName = AnsiConsole.Prompt(prompt);
+        }
+
+        return new FileInfo(solutionsFiles[solutionName]);
+    }
+}
