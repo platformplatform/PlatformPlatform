05b26ffa61e7a0bb64799cc86e38a1543ed6a4d5
Thomas Jespersen <tje@platformplatform.net>
2024-06-10 00:58:36 +0200

    Fix warning when running bash script on a new Azure subscription without Microsoft.ContainerService provider registered

--- cloud-infrastructure/cluster/deploy-cluster.sh
+++ cloud-infrastructure/cluster/deploy-cluster.sh
@@ -22,7 +22,7 @@ get_active_version()
 function is_domain_configured() {
   # Get details about the container apps
   local app_details=$(az containerapp show --name "$1" --resource-group "$2" 2>&1)
-  if [[ "$app_details" == *"ResourceNotFound"* ]] || [[ "$app_details" == *"ResourceGroupNotFound"* ]]; then
+  if [[ "$app_details" == *"ResourceNotFound"* ]] || [[ "$app_details" == *"ResourceGroupNotFound"* ]] || [[ "$app_details" == *"ERROR"* ]] ; then
     echo "false"
   else
     local result=$(echo "$app_details" | jq -r '.properties.configuration.ingress.customDomains')
