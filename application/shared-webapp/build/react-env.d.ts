a7084b35a493347647964d829b547c74f60a162e
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-22 21:49:44 +0200

    Downgrade to React 18 canary until RAC supports v19

--- application/shared-webapp/build/react-env.d.ts
+++ application/shared-webapp/build/react-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="@rsbuild/core/types" />
 /// <reference path="./environment.d.ts" />
+
+// React 18 Canary version typings
+/// <reference types="react-dom/canary" />
+/// <reference types="react/canary" />
