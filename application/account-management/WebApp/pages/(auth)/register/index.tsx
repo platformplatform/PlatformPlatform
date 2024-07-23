ece0d144851ec19a90e1c9be09dca19d4f3973aa
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-24 00:22:18 +0200

    Fix auth routes

--- application/account-management/WebApp/pages/(auth)/register/index.tsx
+++ application/account-management/WebApp/pages/(auth)/register/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { HorizontalHeroLayout } from "@/shared/ui/layout/HorizontalHeroLayout";
 import { StartAccountRegistrationForm } from "@/shared/ui/auth/StartAccountRegistrationForm";
 
-export const Route = createFileRoute("/(auth)/register")({
+export const Route = createFileRoute("/(auth)/register/")({
   component: () => (
     <HorizontalHeroLayout>
       <StartAccountRegistrationForm />
