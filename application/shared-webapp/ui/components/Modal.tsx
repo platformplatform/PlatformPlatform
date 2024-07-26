04990af5b8987b3b3a9138f43d627567a0b123a1
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-26 23:30:23 +0200

    Add positioning to Modal

--- application/shared-webapp/ui/components/Modal.tsx
+++ application/shared-webapp/ui/components/Modal.tsx
@@ -6,19 +6,34 @@ import { Modal as AriaModal, ModalOverlay, type ModalOverlayProps } from "react-
 import { tv } from "tailwind-variants";
 
 const overlayStyles = tv({
-  base: "fixed top-0 left-0 w-full h-[--visual-viewport-height] isolate z-20 bg-black/[15%] flex items-center justify-center p-4 text-center backdrop-blur-lg",
+  base: "fixed top-0 left-0 w-full h-[--visual-viewport-height] isolate z-20 bg-black/[15%] text-center flex backdrop-blur-lg",
   variants: {
     isEntering: {
       true: "animate-in fade-in duration-200 ease-out"
     },
     isExiting: {
       true: "animate-out fade-out duration-200 ease-in"
+    },
+    position: {
+      center: "items-center justify-center",
+      top: "items-start justify-center",
+      left: "justify-start items-stretch",
+      right: "justify-end items-stretch",
+      bottom: "items-end justify-center"
+    },
+    fullSize: {
+      true: "",
+      false: "p-4"
     }
+  },
+  defaultVariants: {
+    position: "center",
+    fullSize: false
   }
 });
 
 const modalStyles = tv({
-  base: "w-fit rounded-2xl bg-white dark:bg-zinc-800/70 dark:backdrop-blur-2xl dark:backdrop-saturate-200 forced-colors:bg-[Canvas] text-left align-middle text-slate-700 dark:text-zinc-300 shadow-2xl bg-clip-padding border border-black/10 dark:border-white/10",
+  base: "w-fit rounded-lg bg-popover dark:backdrop-blur-2xl dark:backdrop-saturate-200 forced-colors:bg-[Canvas] text-left align-middle text-foreground shadow-2xl bg-clip-padding border border-border",
   variants: {
     isEntering: {
       true: "animate-in zoom-in-105 ease-out duration-200"
@@ -29,9 +44,14 @@ const modalStyles = tv({
   }
 });
 
-export function Modal(props: Readonly<ModalOverlayProps>) {
+type ModalProps = {
+  position?: "center" | "top" | "left" | "right" | "bottom";
+  fullSize?: boolean;
+} & ModalOverlayProps;
+
+export function Modal({ position, fullSize, ...props }: Readonly<ModalProps>) {
   return (
-    <ModalOverlay {...props} className={overlayStyles}>
+    <ModalOverlay {...props} className={(renderProps) => overlayStyles({ position, fullSize, ...renderProps })}>
       <AriaModal {...props} className={modalStyles} />
     </ModalOverlay>
   );
