eaa75487d872e87694ba8c741884abeeccc21789
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-07-30 21:52:14 +0200

    Add and configure AuthenticationMiddleware in api client

--- application/account-management/WebApp/shared/lib/api/client.ts
+++ application/account-management/WebApp/shared/lib/api/client.ts
@@ -1,8 +1,11 @@
 import type { paths, components } from "./api.generated";
 import { createPlatformApiClient } from "@repo/infrastructure/api/PlatformApiClient";
+import { createAuthenticationMiddleware } from "@repo/infrastructure/auth/AuthenticationMiddleware";
 export * from "./api.generated.d";
 
 export const api = createPlatformApiClient<paths>();
+api.addMiddleware(createAuthenticationMiddleware({ loginPath: "/login" }));
+
 export const useApi = api.useApi;
 
 export type Schemas = components["schemas"];
---
+++ application/shared-webapp/infrastructure/auth/AuthenticationMiddleware.ts
@@ -0,0 +1,16 @@
+import type { Middleware } from "openapi-fetch";
+
+type AuthenticationMiddlewareOptions = {
+  loginPath: string;
+};
+export function createAuthenticationMiddleware({ loginPath }: AuthenticationMiddlewareOptions): Middleware {
+  return {
+    onResponse(options) {
+      if (options.response.status === 401) {
+        const redirectUrl = new URL(loginPath, window.location.href);
+        redirectUrl.searchParams.set("returnUrl", window.location.href);
+        window.location.href = redirectUrl.href;
+      }
+    }
+  };
+}
