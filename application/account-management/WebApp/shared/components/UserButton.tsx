1d228204658874d2455cdce64d5469c74b588f8f
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-08-02 14:14:58 +0200

    Add Avatar props for className and size

--- application/account-management/WebApp/shared/components/UserButton.tsx
+++ application/account-management/WebApp/shared/components/UserButton.tsx
@@ -5,10 +5,12 @@ import { LogOutIcon, SettingsIcon, UserIcon } from "lucide-react";
 import AccountModal from "@/shared/components/accountModals/AccountSettingsModal";
 import UserProfileModal from "@/shared/components/userModals/UserProfileModal";
 import DeleteAccountModal from "@/shared/components/accountModals/DeleteAccountConfirmation";
-import { Avatar } from "@repo/ui/components/Avatar";
+import { Avatar, type AvatarProps } from "@repo/ui/components/Avatar";
 import { useUserInfo } from "@repo/infrastructure/auth/hooks";
 
-export function UserButton() {
+export type UserButtonProps = Pick<AvatarProps, "size" | "isRound">;
+
+export function UserButton({ isRound = false, size = "xs" }: Readonly<UserButtonProps>) {
   const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
   const [isAccountModalOpen, setIsAccountModalOpen] = useState(false);
   const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] = useState(false);
@@ -19,13 +21,13 @@ export function UserButton() {
   return (
     <>
       <MenuTrigger aria-label="account settings">
-        <Button aria-label="Menu" variant="icon" className="rounded-full">
-          <Avatar avatarUrl={userInfo.avatarUrl} initials={userInfo.initials} isRound size="sm" />
+        <Button aria-label="Menu" variant="icon" className={isRound ? "rounded-full" : "rounded-md"}>
+          <Avatar avatarUrl={userInfo.avatarUrl} initials={userInfo.initials} isRound={isRound} size={size} />
         </Button>
         <Menu placement="bottom end">
           <MenuItem onAction={() => setIsProfileModalOpen(true)}>
             <div className="flex flex-row items-center gap-2">
-              <Avatar avatarUrl={userInfo.avatarUrl} initials={userInfo.initials} isRound size="sm" />
+              <Avatar avatarUrl={userInfo.avatarUrl} initials={userInfo.initials} isRound={isRound} size={size} />
               <div className="flex flex-col">
                 <h2>{userInfo.fullName}</h2>
                 <p className="text-muted-foreground text-sm font-normal">{userInfo.title}</p>
