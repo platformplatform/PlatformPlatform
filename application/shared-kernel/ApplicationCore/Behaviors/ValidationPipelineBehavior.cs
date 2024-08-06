33b0fc7d5879c1652d7f1d789ade569126f23ef3
Thomas Jespersen <tje@platformplatform.net>
2024-08-06 21:08:52 +0200

    Update SharedKernel to allow input validation of queries

--- application/shared-kernel/ApplicationCore/Behaviors/ValidationPipelineBehavior.cs
+++ application/shared-kernel/ApplicationCore/Behaviors/ValidationPipelineBehavior.cs
@@ -11,7 +11,7 @@ namespace PlatformPlatform.SharedKernel.ApplicationCore.Behaviors;
 ///     handled. If the request is valid, the next pipeline behavior will be called.
 /// </summary>
 public sealed class ValidationPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
-    : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand where TResponse : ResultBase
+    : IPipelineBehavior<TRequest, TResponse> where TResponse : ResultBase where TRequest : class
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
