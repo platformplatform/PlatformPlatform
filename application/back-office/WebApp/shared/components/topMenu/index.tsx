7cf58a71ec516fce291da868579cf3c75b5768f4
Thomas Jespersen <tje@platformplatform.net>
2024-08-27 22:07:15 +0200

    Create UI for BackOffice using the same layout as the Admin center

---
+++ application/back-office/WebApp/shared/components/topMenu/index.tsx
@@ -0,0 +1,34 @@
+import { ThemeModeSelector } from "@repo/ui/theme/ThemeModeSelector";
+import { LocaleSwitcher } from "@repo/infrastructure/translations/LocaleSwitcher";
+import { Breadcrumb, Breadcrumbs } from "@repo/ui/components/Breadcrumbs";
+import { Button } from "@repo/ui/components/Button";
+import { LifeBuoyIcon } from "lucide-react";
+import type { ReactNode } from "react";
+import { lazy } from "react";
+
+const AvatarButton = lazy(() => import("account-management/AvatarButton"));
+
+interface TopMenuProps {
+  children?: ReactNode;
+}
+
+export function TopMenu({ children }: Readonly<TopMenuProps>) {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <Breadcrumbs>
+        <Breadcrumb>Home</Breadcrumb>
+        {children}
+      </Breadcrumbs>
+      <div className="flex flex-row gap-6 items-center">
+        <span className="hidden sm:flex">
+          <ThemeModeSelector />
+          <Button variant="icon">
+            <LifeBuoyIcon size={20} />
+          </Button>
+          <LocaleSwitcher />
+        </span>
+        <AvatarButton />
+      </div>
+    </div>
+  );
+}
