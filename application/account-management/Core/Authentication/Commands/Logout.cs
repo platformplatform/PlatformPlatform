451ed145f1a6cb516dcbdac567f5ebc9b6f85879
Thomas Jespersen <tje@platformplatform.net>
2024-08-30 14:11:24 +0200

    Fix spelling of Logout.cs and LogoutHandler

--- application/account-management/Core/Authentication/Commands/Logout.cs
+++ application/account-management/Core/Authentication/Commands/Logout.cs
@@ -11,7 +11,7 @@ namespace PlatformPlatform.AccountManagement.Core.Authentication.Commands;
 public sealed record LogoutCommand
     : ICommand, IRequest<Result>;
 
-public sealed class LogutHandler(
+public sealed class LogoutHandler(
     AuthenticationTokenService authenticationTokenService,
     IHttpContextAccessor httpContextAccessor,
     ITelemetryEventsCollector events,
