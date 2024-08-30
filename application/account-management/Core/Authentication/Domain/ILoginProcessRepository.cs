a691d149e67f2ecadd7f5052daeabc718a83331c
Thomas Jespersen <tje@platformplatform.net>
2024-08-30 14:14:26 +0200

    Move IRepository interfaces into the corresponding repository file for higher cohesion

--- application/account-management/Core/Authentication/Domain/ILoginProcessRepository.cs
+++
@@ -1,5 +0,0 @@
-using PlatformPlatform.SharedKernel.Entities;
-
-namespace PlatformPlatform.AccountManagement.Core.Authentication.Domain;
-
-public interface ILoginRepository : ICrudRepository<Login, LoginId>;
--- application/account-management/Core/Signups/Domain/ISignupRepository.cs
+++
@@ -1,9 +0,0 @@
-using PlatformPlatform.AccountManagement.Core.Tenants.Domain;
-using PlatformPlatform.SharedKernel.Entities;
-
-namespace PlatformPlatform.AccountManagement.Core.Signups.Domain;
-
-public interface ISignupRepository : ICrudRepository<Signup, SignupId>
-{
-    Signup[] GetByEmailOrTenantId(TenantId tenantId, string email);
-}
