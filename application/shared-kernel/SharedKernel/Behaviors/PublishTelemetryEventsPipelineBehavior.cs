f94599889dd547dc581c45e6dea7b58a535c7d0b
Thomas Jespersen <tje@platformplatform.net>
2024-11-07 21:46:45 +0100

    Update Telemetry to always include TenantId, UserId, IsAuthenticated, UserLocale, and UserRole

--- application/shared-kernel/SharedKernel/Behaviors/PublishTelemetryEventsPipelineBehavior.cs
+++ application/shared-kernel/SharedKernel/Behaviors/PublishTelemetryEventsPipelineBehavior.cs
@@ -1,5 +1,6 @@
 using Microsoft.ApplicationInsights;
 using PlatformPlatform.SharedKernel.Cqrs;
+using PlatformPlatform.SharedKernel.ExecutionContext;
 using PlatformPlatform.SharedKernel.TelemetryEvents;
 
 namespace PlatformPlatform.SharedKernel.Behaviors;
@@ -7,7 +8,8 @@ namespace PlatformPlatform.SharedKernel.Behaviors;
 public sealed class PublishTelemetryEventsPipelineBehavior<TRequest, TResponse>(
     ITelemetryEventsCollector telemetryEventsCollector,
     TelemetryClient telemetryClient,
-    ConcurrentCommandCounter concurrentCommandCounter
+    ConcurrentCommandCounter concurrentCommandCounter,
+    IExecutionContext executionContext
 ) : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand where TResponse : ResultBase
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
@@ -19,10 +21,24 @@ public sealed class PublishTelemetryEventsPipelineBehavior<TRequest, TResponse>(
             while (telemetryEventsCollector.HasEvents)
             {
                 var telemetryEvent = telemetryEventsCollector.Dequeue();
+
+                AddPropertyIfNotNull(telemetryEvent, "tenant_TenantId", executionContext.TenantId);
+                AddPropertyIfNotNull(telemetryEvent, "user_IsAuthenticated", executionContext.UserInfo.IsAuthenticated.ToString());
+                AddPropertyIfNotNull(telemetryEvent, "user_UserId", executionContext.UserInfo.UserId);
+                AddPropertyIfNotNull(telemetryEvent, "user_Locale", executionContext.UserInfo.Locale);
+                AddPropertyIfNotNull(telemetryEvent, "user_UserRole", executionContext.UserInfo.UserRole);
+
                 telemetryClient.TrackEvent(telemetryEvent.GetType().Name, telemetryEvent.Properties);
             }
         }
 
         return response;
+    }
+
+    private static void AddPropertyIfNotNull(TelemetryEvent telemetryEvent, string name, object? value)
+    {
+        var stringValue = value?.ToString();
+        if (stringValue is null) return;
+        telemetryEvent.Properties.Add(name, stringValue);
     }
 }
