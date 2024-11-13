d285b0d8ca77d5cb4817fbdf3fb1363e153c6ad0
Thomas Jespersen <tje@platformplatform.net>
2024-11-13 14:42:54 +0100

    Extend IExecutionContext to collect Client IP Address for location tracking

--- application/shared-kernel/SharedKernel/ExecutionContext/HttpExecutionContext.cs
+++ application/shared-kernel/SharedKernel/ExecutionContext/HttpExecutionContext.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using PlatformPlatform.SharedKernel.Authentication;
@@ -7,6 +8,7 @@ namespace PlatformPlatform.SharedKernel.ExecutionContext;
 
 public class HttpExecutionContext(IHttpContextAccessor httpContextAccessor) : IExecutionContext
 {
+    private IPAddress? _clientIpAddress;
     private bool _isTenantIdCalculated;
     private TenantId? _tenantId;
     private UserInfo? _userInfo;
@@ -38,6 +40,30 @@ public class HttpExecutionContext(IHttpContextAccessor httpContextAccessor) : IE
             var browserLocale = httpContextAccessor.HttpContext?.Features.Get<IRequestCultureFeature>()?.RequestCulture.Culture.Name;
 
             return _userInfo = UserInfo.Create(httpContextAccessor.HttpContext?.User, browserLocale);
+        }
+    }
+
+    public IPAddress ClientIpAddress
+    {
+        get
+        {
+            if (_clientIpAddress is not null)
+            {
+                return _clientIpAddress;
+            }
+
+            if (httpContextAccessor.HttpContext == null)
+            {
+                return _clientIpAddress = IPAddress.None;
+            }
+
+            var forwardedIps = httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"].ToString().Split(',');
+            if (IPAddress.TryParse(forwardedIps.LastOrDefault(), out var clientIpAddress))
+            {
+                return _clientIpAddress = clientIpAddress;
+            }
+
+            return _clientIpAddress = IPAddress.None;
         }
     }
 }
