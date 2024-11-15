7e6289b55353f087ddbc431f06f2b36539f1b472
Thomas Jespersen <tje@platformplatform.net>
2024-11-15 23:18:31 +0100

    Remove retry logic from database migration as Aspire ensures SQL Server readiness

--- application/shared-kernel/SharedKernel/Database/DatabaseMigrationService.cs
+++ application/shared-kernel/SharedKernel/Database/DatabaseMigrationService.cs
@@ -1,44 +1,16 @@
-using System.Net.Sockets;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace PlatformPlatform.SharedKernel.Database;
 
-public class DatabaseMigrationService<T>(T dbContext, ILogger<DatabaseMigrationService<T>> logger)
-    where T : DbContext
+public class DatabaseMigrationService<TContext>(TContext dbContext, ILogger<DatabaseMigrationService<TContext>> logger)
+    where TContext : DbContext
 {
     public void ApplyMigrations()
     {
-        var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
-        logger.LogInformation("Applying database migrations. Version: {Version}", version);
+        logger.LogInformation("Applying database migrations. Version: {Version}",
+            Assembly.GetExecutingAssembly().GetName().Version
+        );
 
-        const int maxRetryCount = 30;
-        for (var retry = 1; retry <= maxRetryCount; retry++)
-        {
-            try
-            {
-                if (retry % 5 == 0) logger.LogInformation("Waiting for databases to be ready...");
-
-                var executionStrategy = dbContext.Database.CreateExecutionStrategy();
-
-                executionStrategy.Execute(() => dbContext.Database.Migrate());
-
-                logger.LogInformation("Finished migrating database.");
-
-                return;
-            }
-            catch (SqlException ex) when (ex.Message.Contains("an error occurred during the pre-login handshake"))
-            {
-                // Known error in Aspire, when SQL Server is not ready
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
-            catch (SocketException ex) when (ex.Message.Contains("Invalid argument"))
-            {
-                // Known error in Aspire, when SQL Server is not ready
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
-        }
-
-        throw new TimeoutException($"Database migration failed after {maxRetryCount} retries.");
+        dbContext.Database.CreateExecutionStrategy().Execute(() => { dbContext.Database.Migrate(); });
     }
 }
