3e6730c6461a56f715a83d4c76188904e91af5c4
Thomas Jespersen <tje@platformplatform.net>
2025-01-25 22:56:44 +0100

    Add GetCurrentTenantAsync method to TenantRepository

--- application/account-management/Core/Features/Tenants/Domain/TenantRepository.cs
+++ application/account-management/Core/Features/Tenants/Domain/TenantRepository.cs
@@ -1,20 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformPlatform.AccountManagement.Database;
 using PlatformPlatform.SharedKernel.Domain;
+using PlatformPlatform.SharedKernel.ExecutionContext;
 using PlatformPlatform.SharedKernel.Persistence;
 
 namespace PlatformPlatform.AccountManagement.Features.Tenants.Domain;
 
 public interface ITenantRepository : ICrudRepository<Tenant, TenantId>
 {
+    Task<Tenant> GetCurrentTenantAsync(CancellationToken cancellationToken);
+
     Task<bool> ExistsAsync(TenantId id, CancellationToken cancellationToken);
 
     Task<bool> IsSubdomainFreeAsync(string subdomain, CancellationToken cancellationToken);
 }
 
-internal sealed class TenantRepository(AccountManagementDbContext accountManagementDbContext)
+internal sealed class TenantRepository(AccountManagementDbContext accountManagementDbContext, IExecutionContext executionContext)
     : RepositoryBase<Tenant, TenantId>(accountManagementDbContext), ITenantRepository
 {
+    public async Task<Tenant> GetCurrentTenantAsync(CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(executionContext.TenantId!);
+        return await GetByIdAsync(executionContext.TenantId, cancellationToken) ??
+               throw new InvalidOperationException("Active tenant not found.");
+    }
+
     public Task<bool> IsSubdomainFreeAsync(string subdomain, CancellationToken cancellationToken)
     {
         return DbSet.AllAsync(tenant => tenant.Id != subdomain, cancellationToken);
