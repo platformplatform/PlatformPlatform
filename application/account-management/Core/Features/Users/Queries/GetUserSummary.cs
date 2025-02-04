5d0a5b7e9e75c9aa37fde8af7e2a080c82c9582d
Thomas Jespersen <tje@platformplatform.net>
2025-02-04 09:58:49 +0100

    Fix naming of Query responses

--- application/account-management/Core/Features/Users/Queries/GetUserSummary.cs
+++ application/account-management/Core/Features/Users/Queries/GetUserSummary.cs
@@ -5,17 +5,17 @@ using PlatformPlatform.SharedKernel.Cqrs;
 namespace PlatformPlatform.AccountManagement.Features.Users.Queries;
 
 [PublicAPI]
-public sealed record GetUserSummaryQuery : IRequest<Result<GetUserSummaryResponse>>;
+public sealed record GetUserSummaryQuery : IRequest<Result<UserSummaryResponse>>;
 
 [PublicAPI]
-public sealed record GetUserSummaryResponse(int TotalUsers, int ActiveUsers, int PendingUsers);
+public sealed record UserSummaryResponse(int TotalUsers, int ActiveUsers, int PendingUsers);
 
 public sealed class GetUserSummaryHandler(IUserRepository userRepository)
-    : IRequestHandler<GetUserSummaryQuery, Result<GetUserSummaryResponse>>
+    : IRequestHandler<GetUserSummaryQuery, Result<UserSummaryResponse>>
 {
-    public async Task<Result<GetUserSummaryResponse>> Handle(GetUserSummaryQuery query, CancellationToken cancellationToken)
+    public async Task<Result<UserSummaryResponse>> Handle(GetUserSummaryQuery query, CancellationToken cancellationToken)
     {
         var (totalUsers, activeUsers, pendingUsers) = await userRepository.GetUserSummaryAsync(cancellationToken);
-        return new GetUserSummaryResponse(totalUsers, activeUsers, pendingUsers);
+        return new UserSummaryResponse(totalUsers, activeUsers, pendingUsers);
     }
 }
