c24d8a74e1a3057f6752c68ae605c3ca068a6bab
Guðmundur Hreiðarsson <gudmundur.hreidarsson@gmail.com>
2025-02-09 09:19:53 +0100

    Set up TanStack Query API client alongside the old one

--- application/account-management/WebApp/shared/lib/api/client.ts
+++ application/account-management/WebApp/shared/lib/api/client.ts
@@ -2,11 +2,20 @@ import type { components, paths } from "./api.generated";
 import { createPlatformApiClient } from "@repo/infrastructure/api/PlatformApiClient";
 import { createAuthenticationMiddleware } from "@repo/infrastructure/auth/AuthenticationMiddleware";
 
+import createFetchClient from "openapi-fetch";
+import createClient from "openapi-react-query";
+
 export * from "./api.generated.d";
 
 export const api = createPlatformApiClient<paths>();
 api.addMiddleware(createAuthenticationMiddleware());
 
 export const useApi = api.useApi;
+
+export const apiClient = createFetchClient<paths>({
+  baseUrl: import.meta.env.PUBLIC_URL
+});
+
+export const newApi = createClient(apiClient);
 
 export type Schemas = components["schemas"];
