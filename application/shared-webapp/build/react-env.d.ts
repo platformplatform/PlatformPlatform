f9ad9a9d85a31210d5cede58d2c30d34e29a1eff
Thomas Jespersen <tje@platformplatform.net>
2025-03-08 13:13:24 +0100

    Remove leftover configuration from React Canary usage

--- application/shared-webapp/build/react-env.d.ts
+++ application/shared-webapp/build/react-env.d.ts
@@ -1,6 +1,2 @@
 /// <reference types="@rsbuild/core/types" />
 /// <reference path="./environment.d.ts" />
-
-// React 18 Canary version typings
-/// <reference types="react-dom/canary" />
-/// <reference types="react/canary" />
