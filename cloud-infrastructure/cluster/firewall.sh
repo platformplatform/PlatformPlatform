c784631d4c53b807fc70b7e193cc0747d34a268b
Thomas Jespersen <tje@platformplatform.net>
2025-03-23 19:35:00 +0100

    Update Open/Close firewall script to scope firewall rules at the database level, allowing multiple GitHub workflows to run in parallel

--- cloud-infrastructure/cluster/firewall.sh
+++ cloud-infrastructure/cluster/firewall.sh
@@ -1,11 +1,11 @@
 IP_ADDRESS=$(curl -s https://api.ipify.org)
-FIREWALL_RULE_NAME="GitHub Action Workflows - Only active when deploying"
+FIREWALL_RULE_NAME="GitHub Action Workflows - ${SQL_DATABASE_NAME} - Only active when deploying"
 
 if [[ "$1" == "open" ]]
 then
-    echo "$(date +"%Y-%m-%dT%H:%M:%S") Add the IP $IP_ADDRESS to the SQL Server firewall on server $SQL_SERVER_NAME"
+    echo "$(date +"%Y-%m-%dT%H:%M:%S") Add the IP $IP_ADDRESS to the SQL Server firewall on server $SQL_SERVER_NAME for database $SQL_DATABASE_NAME"
     az sql server firewall-rule create --resource-group $RESOURCE_GROUP_NAME --server $SQL_SERVER_NAME --name "$FIREWALL_RULE_NAME" --start-ip-address $IP_ADDRESS --end-ip-address $IP_ADDRESS
 else
-    echo "$(date +"%Y-%m-%dT%H:%M:%S") Delete the IP $IP_ADDRESS from the SQL Server firewall on server $SQL_SERVER_NAME"
+    echo "$(date +"%Y-%m-%dT%H:%M:%S") Delete the IP $IP_ADDRESS from the SQL Server firewall on server $SQL_SERVER_NAME for database $SQL_DATABASE_NAME"
     az sql server firewall-rule delete --resource-group $RESOURCE_GROUP_NAME --server $SQL_SERVER_NAME --name "$FIREWALL_RULE_NAME"
 fi
