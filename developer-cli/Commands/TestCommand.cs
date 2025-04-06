d27b83f0de02c7cc4bc3c78aa464ec22afa3b622
Thomas Jespersen <tje@platformplatform.net>
2025-04-06 13:22:57 +0200

    Add option to run test CLI command without building and restoring

--- developer-cli/Commands/TestCommand.cs
+++ developer-cli/Commands/TestCommand.cs
@@ -10,16 +10,22 @@ public class TestCommand : Command
     public TestCommand() : base("test", "Runs tests from a solution")
     {
         AddOption(new Option<string?>(["<solution-name>", "--solution-name", "-s"], "The name of the solution file containing the tests to run"));
+        AddOption(new Option<bool>(["--no-build"], () => false, "Skip building and restoring the solution before running tests"));
 
-        Handler = CommandHandler.Create<string?>(Execute);
+        Handler = CommandHandler.Create<string?, bool>(Execute);
     }
 
-    private void Execute(string? solutionName)
+    private void Execute(string? solutionName, bool noBuild)
     {
         Prerequisite.Ensure(Prerequisite.Dotnet);
 
         var solutionFile = SolutionHelper.GetSolution(solutionName);
 
-        ProcessHelper.StartProcess($"dotnet test {solutionFile.Name}", solutionFile.Directory?.FullName);
+        if (!noBuild)
+        {
+            ProcessHelper.StartProcess($"dotnet build {solutionFile.Name}", solutionFile.Directory?.FullName);
+        }
+
+        ProcessHelper.StartProcess($"dotnet test {solutionFile.Name} --no-build --no-restore", solutionFile.Directory?.FullName);
     }
 }
