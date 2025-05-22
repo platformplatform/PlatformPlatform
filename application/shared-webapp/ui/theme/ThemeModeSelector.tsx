3f53361c322b16b000f0a0628a43820bd079aad2
Thomas Jespersen <tje@platformplatform.net>
2025-05-22 11:46:10 +0200

    Introduce 3-state theme button switching between system, dark, and light theme

--- application/shared-webapp/ui/theme/ThemeModeSelector.tsx
+++ application/shared-webapp/ui/theme/ThemeModeSelector.tsx
@@ -3,10 +3,11 @@ import { MoonIcon, MoonStarIcon, SunIcon, SunMoonIcon } from "lucide-react";
 import { toggleThemeMode, useThemeMode } from "./mode/ThemeMode";
 
 /**
- * A button that toggles the theme mode between light and dark.
+ * A button that toggles the theme mode between system, light and dark.
  */
 export function ThemeModeSelector({ "aria-label": ariaLabel }: { "aria-label": string }) {
   const { setThemeMode } = useThemeMode();
+
   return (
     <Button variant="icon" onPress={() => setThemeMode(toggleThemeMode)} aria-label={ariaLabel}>
       <ThemeModeIcon />
@@ -16,14 +17,9 @@ export function ThemeModeSelector({ "aria-label": ariaLabel }: { "aria-label": s
 
 function ThemeModeIcon() {
   const { themeMode, resolvedThemeMode } = useThemeMode();
-  if (themeMode === "light") {
-    return <SunIcon className="h-4 w-4" />;
+
+  if (resolvedThemeMode === "dark") {
+    return themeMode === "system" ? <MoonStarIcon className="h-4 w-4" /> : <MoonIcon className="h-4 w-4" />;
   }
-  if (themeMode === "dark") {
-    return <MoonIcon className="h-4 w-4" />;
-  }
-  if (resolvedThemeMode === "light") {
-    return <SunMoonIcon className="h-4 w-4" />;
-  }
-  return <MoonStarIcon className="h-4 w-4" />;
+  return themeMode === "system" ? <SunMoonIcon className="h-4 w-4" /> : <SunIcon className="h-4 w-4" />;
 }
