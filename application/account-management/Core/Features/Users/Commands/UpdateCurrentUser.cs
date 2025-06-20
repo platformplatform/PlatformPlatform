5bb4d2854b6fed31761d9a5b3241389ff24c2b4f
Thomas Jespersen <tje@platformplatform.net>
2025-06-20 14:20:50 +0200

    Remove email from UpdateCurrentUserCommand and disable email TextField in UserProfile dialog; add email icon to the field

--- application/account-management/Core/Features/Users/Commands/UpdateCurrentUser.cs
+++ application/account-management/Core/Features/Users/Commands/UpdateCurrentUser.cs
@@ -8,11 +8,9 @@ using PlatformPlatform.SharedKernel.Validation;
 namespace PlatformPlatform.AccountManagement.Features.Users.Commands;
 
 [PublicAPI]
-public sealed record UpdateCurrentUserCommand(string Email, string FirstName, string LastName, string Title)
+public sealed record UpdateCurrentUserCommand(string FirstName, string LastName, string Title)
     : ICommand, IRequest<Result>
 {
-    public string Email { get; } = Email.ToLower().Trim();
-
     public string FirstName { get; } = FirstName.Trim();
 
     public string LastName { get; } = LastName.Trim();
@@ -24,7 +22,6 @@ public sealed class UpdateCurrentUserValidator : AbstractValidator<UpdateCurrent
 {
     public UpdateCurrentUserValidator()
     {
-        RuleFor(x => x.Email).SetValidator(new SharedValidations.Email());
         RuleFor(x => x.FirstName).Length(1, 30).WithMessage("First name must be between 1 and 30 characters.");
         RuleFor(x => x.LastName).Length(1, 30).WithMessage("Last name must be between 1 and 30 characters.");
         RuleFor(x => x.Title).MaximumLength(50).WithMessage("Title must be no longer than 50 characters.");
@@ -37,8 +34,7 @@ public sealed class UpdateCurrentUserHandler(IUserRepository userRepository, ITe
     public async Task<Result> Handle(UpdateCurrentUserCommand command, CancellationToken cancellationToken)
     {
         var user = await userRepository.GetLoggedInUserAsync(cancellationToken);
-
-        user.UpdateEmail(command.Email);
+        
         user.Update(command.FirstName, command.LastName, command.Title);
         userRepository.Update(user);
 
