903a3c6ed62fc7d453be7c9eaa277dfa3481f369
Thomas Jespersen <tje@platformplatform.net>
2025-06-23 09:16:36 +0200

    Reduce padding in mobile device dialogs

--- application/shared-webapp/ui/components/Dialog.tsx
+++ application/shared-webapp/ui/components/Dialog.tsx
@@ -8,7 +8,7 @@ export function Dialog(props: Readonly<DialogProps>) {
     <AriaDialog
       {...props}
       className={twMerge(
-        "relative max-h-full min-h-0 overflow-y-auto overscroll-contain p-11 outline outline-0 [[data-placement]>&]:p-4",
+        "relative max-h-full min-h-0 overflow-y-auto overscroll-contain p-4 outline outline-0 sm:p-11 [[data-placement]>&]:p-4",
         props.className
       )}
     />
