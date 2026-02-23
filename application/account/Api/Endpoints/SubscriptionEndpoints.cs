eccb0d2a0bc296a5ac9ec43836ea9709332a5544
Thomas Jespersen <tje@platformplatform.net>
2026-02-23 11:35:56 +0100

    Organize subscription endpoints in logical call order aligned with subscription lifecycle

--- application/account/Api/Endpoints/SubscriptionEndpoints.cs
+++ application/account/Api/Endpoints/SubscriptionEndpoints.cs
@@ -13,37 +13,49 @@ public sealed class SubscriptionEndpoints : IEndpoints
     {
         var group = routes.MapGroup(RoutesPrefix).WithTags("Subscriptions").RequireAuthorization().ProducesValidationProblem();
 
+        group.MapGet("/pricing-catalog", async Task<ApiResult<PricingCatalogResponse>> ([AsParameters] GetPricingCatalogQuery query, IMediator mediator)
+            => await mediator.Send(query)
+        ).Produces<PricingCatalogResponse>();
+
         group.MapGet("/current", async Task<ApiResult<SubscriptionResponse>> ([AsParameters] GetCurrentSubscriptionQuery query, IMediator mediator)
             => await mediator.Send(query)
         ).Produces<SubscriptionResponse>();
-
-        group.MapGet("/payment-history", async Task<ApiResult<PaymentHistoryResponse>> ([AsParameters] GetPaymentHistoryQuery query, IMediator mediator)
-            => await mediator.Send(query)
-        ).Produces<PaymentHistoryResponse>();
-
-        group.MapGet("/upgrade-preview", async Task<ApiResult<UpgradePreviewResponse>> ([AsParameters] GetUpgradePreviewQuery query, IMediator mediator)
-            => await mediator.Send(query)
-        ).Produces<UpgradePreviewResponse>();
 
         group.MapGet("/checkout-preview", async Task<ApiResult<CheckoutPreviewResponse>> ([AsParameters] GetCheckoutPreviewQuery query, IMediator mediator)
             => await mediator.Send(query)
         ).Produces<CheckoutPreviewResponse>();
 
-        group.MapGet("/pricing-catalog", async Task<ApiResult<PricingCatalogResponse>> ([AsParameters] GetPricingCatalogQuery query, IMediator mediator)
+        group.MapGet("/subscribe-preview", async Task<ApiResult<SubscribePreviewResponse>> ([AsParameters] GetSubscribePreviewQuery query, IMediator mediator)
             => await mediator.Send(query)
-        ).Produces<PricingCatalogResponse>();
+        ).Produces<SubscribePreviewResponse>();
+
+        group.MapGet("/upgrade-preview", async Task<ApiResult<UpgradePreviewResponse>> ([AsParameters] GetUpgradePreviewQuery query, IMediator mediator)
+            => await mediator.Send(query)
+        ).Produces<UpgradePreviewResponse>();
+
+        group.MapGet("/payment-history", async Task<ApiResult<PaymentHistoryResponse>> ([AsParameters] GetPaymentHistoryQuery query, IMediator mediator)
+            => await mediator.Send(query)
+        ).Produces<PaymentHistoryResponse>();
+
+        group.MapPut("/billing-info", async Task<ApiResult> (UpdateBillingInfoCommand command, IMediator mediator)
+            => await mediator.Send(command)
+        );
 
         group.MapPost("/start-checkout", async Task<ApiResult<StartSubscriptionCheckoutResponse>> (StartSubscriptionCheckoutCommand command, IMediator mediator)
             => await mediator.Send(command)
         ).Produces<StartSubscriptionCheckoutResponse>();
 
+        group.MapPost("/start-payment-method-setup", async Task<ApiResult<StartPaymentMethodSetupResponse>> (IMediator mediator)
+            => await mediator.Send(new StartPaymentMethodSetupCommand())
+        ).Produces<StartPaymentMethodSetupResponse>();
+
+        group.MapPost("/confirm-payment-method", async Task<ApiResult<ConfirmPaymentMethodSetupResponse>> (ConfirmPaymentMethodSetupCommand command, IMediator mediator)
+            => await mediator.Send(command)
+        ).Produces<ConfirmPaymentMethodSetupResponse>();
+
         group.MapPost("/upgrade", async Task<ApiResult<UpgradeSubscriptionResponse>> (UpgradeSubscriptionCommand command, IMediator mediator)
             => await mediator.Send(command)
         ).Produces<UpgradeSubscriptionResponse>();
-
-        group.MapGet("/subscribe-preview", async Task<ApiResult<SubscribePreviewResponse>> ([AsParameters] GetSubscribePreviewQuery query, IMediator mediator)
-            => await mediator.Send(query)
-        ).Produces<SubscribePreviewResponse>();
 
         group.MapPost("/schedule-downgrade", async Task<ApiResult> (ScheduleDowngradeCommand command, IMediator mediator)
             => await mediator.Send(command)
@@ -61,24 +73,12 @@ public sealed class SubscriptionEndpoints : IEndpoints
             => await mediator.Send(new ReactivateSubscriptionCommand())
         ).Produces<ReactivateSubscriptionResponse>();
 
-        group.MapPost("/start-payment-method-setup", async Task<ApiResult<StartPaymentMethodSetupResponse>> (IMediator mediator)
-            => await mediator.Send(new StartPaymentMethodSetupCommand())
-        ).Produces<StartPaymentMethodSetupResponse>();
-
-        group.MapPost("/confirm-payment-method", async Task<ApiResult<ConfirmPaymentMethodSetupResponse>> (ConfirmPaymentMethodSetupCommand command, IMediator mediator)
-            => await mediator.Send(command)
-        ).Produces<ConfirmPaymentMethodSetupResponse>();
-
         group.MapPost("/retry-pending-invoice", async Task<ApiResult<RetryPendingInvoicePaymentResponse>> (IMediator mediator)
             => await mediator.Send(new RetryPendingInvoicePaymentCommand())
         ).Produces<RetryPendingInvoicePaymentResponse>();
 
         group.MapPost("/process-pending-events", async Task<ApiResult> (IMediator mediator)
             => await mediator.Send(new ProcessPendingEventsCommand())
-        );
-
-        group.MapPut("/billing-info", async Task<ApiResult> (UpdateBillingInfoCommand command, IMediator mediator)
-            => await mediator.Send(command)
         );
     }
 }
