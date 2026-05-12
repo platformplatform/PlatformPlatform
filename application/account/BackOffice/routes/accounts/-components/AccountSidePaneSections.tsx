c7f70c69f6c693e69d1f0730d45c951bd03ce161
Thomas Jespersen <tje@platformplatform.net>
2026-05-12 17:08:41 +0200

    Rename side pane Renewal date label to Expires for consistency with detail page

--- application/account/BackOffice/routes/accounts/-components/AccountSidePaneSections.tsx
+++ application/account/BackOffice/routes/accounts/-components/AccountSidePaneSections.tsx
@@ -79,7 +79,7 @@ export function AccountSidePaneSections({
       <SidePaneSection label={t`Plan & revenue`} className="h-[12.875rem]">
         <div className="flex flex-col gap-3">
           <KpiRow
-            leftLabel={t`Renewal date`}
+            leftLabel={t`Expires`}
             leftValue={tenant.renewalDate ? formatDate(tenant.renewalDate) : "-"}
             rightLabel={t`MRR`}
             rightValue={
